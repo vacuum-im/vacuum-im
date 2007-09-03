9656dd89f74cf4f8a13a9c6b26c8ef240aa6f23f
potapov.s.a <potapov.s.a@8ee3b2d0-dc07-11de-995a-ab19b33c1a40>
2007-09-03 12:29:53 +0000

    Изменен скрипт создания нового плагина.

--- Vacuum/trunk/src/plugins/newplugin.bat
+++ Vacuum/trunk/src/plugins/newplugin.bat
@@ -7,7 +7,12 @@ echo LIBS += -l../../libs/utils >> %1.pro
 echo TARGET = %1 >> %1.pro
 echo include(%1.pri) >> %1.pro
 
-echo HEADERS = >> %1.pri
-echo SOURCES = >> %1.pri
+echo HEADERS = %1.h>> %1.pri
+echo SOURCES = %1.cpp>> %1.pri
+
+echo #ifndef %1_H >> %1.h
+echo #define %1_H >> %1.h
+echo #endif >> %1.h
+echo #include "%1.h" >> %1.cpp
 
 qmake -tp vc -o %1.vcproj %1.pro 
