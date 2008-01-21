1ed8d0896a7d7239c09b5db47e5133f78f1137c4
potapov.s.a <potapov.s.a@8ee3b2d0-dc07-11de-995a-ab19b33c1a40>
2008-01-21 14:03:32 +0000

    Добавлен namespace NS_JABBER_DATA из DataForms.

--- Vacuum/trunk/src/definations/namespaces.h
+++ Vacuum/trunk/src/definations/namespaces.h
@@ -12,6 +12,7 @@
 #define NS_JABBER_VERSION                       "jabber:iq:version"
 #define NS_JABBER_LAST                          "jabber:iq:last"
 #define NS_JABBER_DELAY                         "jabber:x:delay"
+#define NS_JABBER_DATA                          "jabber:x:data"
 
 #define NS_FEATURE_IQAUTH                       "http://jabber.org/features/iq-auth"
 #define NS_FEATURE_SASL                         "urn:ietf:params:xml:ns:xmpp-sasl"
