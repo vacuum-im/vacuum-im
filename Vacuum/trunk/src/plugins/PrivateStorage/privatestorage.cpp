a6b2690700f642c3732c596f3251e0a52ff5bdfa
potapov.s.a <potapov.s.a@8ee3b2d0-dc07-11de-995a-ab19b33c1a40>
2008-02-04 11:32:35 +0000

    Изменен интерфейс и принцип работы IPrivateStorage.

--- Vacuum/trunk/src/plugins/PrivateStorage/privatestorage.cpp
+++ Vacuum/trunk/src/plugins/PrivateStorage/privatestorage.cpp
@@ -1,11 +1,12 @@
 #include "privatestorage.h"
 
-#include <QTimer>
+#include <QCryptographicHash>
+
+#define PRIVATE_STORAGE_TIMEOUT       30000
 
 PrivateStorage::PrivateStorage()
 {
-  FRequestId = 0;
-  FChashElem = FCash.appendChild(FCash.createElement("private")).toElement();
+  FStanzaProcessor = NULL;
 }
 
 PrivateStorage::~PrivateStorage()
@@ -27,137 +28,162 @@ void PrivateStorage::pluginInfo(PluginInfo *APluginInfo)
 
 bool PrivateStorage::initConnections(IPluginManager *APluginManager, int &/*AInitOrder*/)
 {
-  IPlugin *plugin = APluginManager->getPlugins("IStanzaProcessor").value(0,NULL);
+  IPlugin *plugin = APluginManager->getPlugins("IXmppStreams").value(0,NULL);
+  if (plugin)
+  {
+    connect(plugin->instance(), SIGNAL(opened(IXmppStream *)), SLOT(onStreamOpened(IXmppStream *))); 
+    connect(plugin->instance(), SIGNAL(closed(IXmppStream *)), SLOT(onStreamClosed(IXmppStream *))); 
+  }
+
+  plugin = APluginManager->getPlugins("IStanzaProcessor").value(0,NULL);
   if (plugin) 
     FStanzaProcessor = qobject_cast<IStanzaProcessor *>(plugin->instance());
 
   return FStanzaProcessor!=NULL;
 }
 
-void PrivateStorage::iqStanza(const Jid &/*AStreamJid*/, const Stanza &AStanza)
+void PrivateStorage::iqStanza(const Jid &AStreamJid, const Stanza &AStanza)
 {
-  if (FIq2Request.contains(AStanza.id()))
+  if (AStanza.type() == "error")
   {
-    int rId = FIq2Request.take(AStanza.id());
-    if (AStanza.type() == "result")
-    {
-      QDomElement elem = AStanza.firstElement("query",NS_JABBER_PRIVATE).firstChildElement();
-      if (!elem.isNull())
-      {
-        QDomElement cashItem = getFromCash(elem.tagName(),elem.namespaceURI());
-        if (!cashItem.isNull())
-          FChashElem.removeChild(cashItem);
-        FChashElem.appendChild(elem.cloneNode(true));
-      }
-      emit requestCompleted(rId);
-    }
-    else
-      emit requestError(rId);
+    FSaveRequests.remove(AStanza.id());
+    FLoadRequests.remove(AStanza.id());
+    FRemoveRequests.remove(AStanza.id());
+    ErrorHandler err(AStanza.element());
+    emit dataError(AStanza.id(),err.message());
+  }
+  else if (FSaveRequests.contains(AStanza.id()))
+  {
+    QDomElement elem = FSaveRequests.take(AStanza.id());
+    emit dataSaved(AStanza.id(),AStreamJid,elem);
+  }
+  else if (FLoadRequests.contains(AStanza.id()))
+  {
+    FLoadRequests.remove(AStanza.id());
+    QDomElement dataElem = AStanza.firstElement("query",NS_JABBER_PRIVATE).firstChildElement();
+    emit dataLoaded(AStanza.id(),AStreamJid,insertElement(AStreamJid,dataElem));
+  }
+  else if (FRemoveRequests.contains(AStanza.id()))
+  {
+    QDomElement dataElem = FRemoveRequests.take(AStanza.id());
+    emit dataRemoved(AStanza.id(),AStreamJid,dataElem);
+    removeElement(AStreamJid,dataElem.tagName(),dataElem.namespaceURI());
   }
 }
 
 void PrivateStorage::iqStanzaTimeOut(const QString &AId)
 {
-  if (FIq2Request.contains(AId))
-  {
-    int rId = FIq2Request.take(AId);
-    emit requestTimeout(rId);
-  }
+  FSaveRequests.remove(AId);
+  FLoadRequests.remove(AId);
+  FRemoveRequests.remove(AId);
+  ErrorHandler err(ErrorHandler::REQUEST_TIMEOUT);
+  emit dataError(AId,err.message());
 }
 
-int PrivateStorage::saveElement(const Jid &AStreamJid, const QDomElement &AElement)
+bool PrivateStorage::hasData(const Jid &AStreamJid, const QString &ATagName, const QString &ANamespace) const
+{
+  return getData(AStreamJid,ATagName,ANamespace).hasChildNodes();
+}
+
+QDomElement PrivateStorage::getData(const Jid &AStreamJid, const QString &ATagName, const QString &ANamespace) const
+{
+  QDomElement elem = FStreamElements.value(AStreamJid).firstChildElement(ATagName);
+  while (!elem.isNull() && elem.namespaceURI()!=ANamespace)
+    elem= elem.nextSiblingElement(ATagName);
+  return elem;
+}
+
+QString PrivateStorage::saveData(const Jid &AStreamJid, const QDomElement &AElement)
 {
   if (AStreamJid.isValid() && !AElement.namespaceURI().isEmpty())
   {
-    QString iqId = FStanzaProcessor->newId();
-
     Stanza stanza("iq");
-    QDomElement elem = stanza.setId(iqId).setType("set").addElement("query",NS_JABBER_PRIVATE);
+    stanza.setType("set").setId(FStanzaProcessor->newId());
+    QDomElement elem = stanza.addElement("query",NS_JABBER_PRIVATE);
     elem.appendChild(AElement.cloneNode(true));
-
-    if (FStanzaProcessor->sendIqStanza(this,AStreamJid,stanza,60000))
+    if (FStanzaProcessor->sendIqStanza(this,AStreamJid,stanza,PRIVATE_STORAGE_TIMEOUT))
     {
-      FRequestId++;
-      FIq2Request.insert(iqId,FRequestId);
-
-      QDomElement elem = getFromCash(AElement.tagName(),AElement.namespaceURI());
-      if (!elem.isNull())
-        FChashElem.removeChild(elem);
-      FChashElem.appendChild(AElement.cloneNode(true));
-
-      return FRequestId;
+      FSaveRequests.insert(stanza.id(),insertElement(AStreamJid,AElement));
+      return stanza.id();
     }
   }
-  return -1;
+  return QString();
 }
 
-int PrivateStorage::loadElement(const Jid &AStreamJid, const QString &ATagName, 
-                                const QString &ANamespace, bool ACheckCash /*= true*/)
+QString PrivateStorage::loadData(const Jid &AStreamJid, const QString &ATagName, const QString &ANamespace)
 {
   if (AStreamJid.isValid() && !ATagName.isEmpty() && !ANamespace.isEmpty())
   {
-    QDomElement elem;
-    if (ACheckCash)
-      elem = getFromCash(ATagName,ANamespace);
-    
-    if (!elem.isNull())
-    {
-      FRequestId++;
-      FCashedRequests.append(FRequestId);
-      QTimer::singleShot(0,this,SLOT(emitCashedRequests()));
-      return FRequestId;
-    }
-    else
-    {
-      QString iqId = FStanzaProcessor->newId();
-      Stanza stanza("iq");
-      QDomElement elem = stanza.setId(iqId).setType("get").addElement("query",NS_JABBER_PRIVATE);
-      elem.appendChild(FCash.createElementNS(ANamespace,ATagName));
-      if (FStanzaProcessor->sendIqStanza(this,AStreamJid,stanza,30000))
-      {
-        FRequestId++;
-        FIq2Request.insert(iqId,FRequestId);
-        return FRequestId;
-      }
-    }
-  }
-  return -1;
-}
-
-int PrivateStorage::removeElement(const Jid &AStreamJid, const QString &ATagName, 
-                                  const QString &ANamespace)
-{
-  if (AStreamJid.isValid() && !ATagName.isEmpty() && !ANamespace.isEmpty())
-  {
-    QString iqId = FStanzaProcessor->newId();
     Stanza stanza("iq");
-    QDomElement elem = stanza.setId(iqId).setType("set").addElement("query",NS_JABBER_PRIVATE);
-    elem.appendChild(FCash.createElementNS(ANamespace,ATagName));
-    if (FStanzaProcessor->sendIqStanza(this,AStreamJid,stanza,30000))
+    stanza.setType("get").setId(FStanzaProcessor->newId());
+    QDomElement elem = stanza.addElement("query",NS_JABBER_PRIVATE);
+    QDomElement dataElem = elem.appendChild(stanza.createElement(ATagName,ANamespace)).toElement();
+    if (FStanzaProcessor->sendIqStanza(this,AStreamJid,stanza,PRIVATE_STORAGE_TIMEOUT))
     {
-      QDomElement elem = getFromCash(ATagName,ANamespace);
-      if (!elem.isNull())
-        FChashElem.removeChild(elem);
-
-      FRequestId++;
-      FIq2Request.insert(iqId,FRequestId);
-      return FRequestId;
+      FLoadRequests.insert(stanza.id(),dataElem);
+      return stanza.id();
     }
   }
-  return -1;
+  return QString();
 }
 
-QDomElement PrivateStorage::getFromCash(const QString &ATagName, const QString &ANamespace) const
+QString PrivateStorage::removeData(const Jid &AStreamJid, const QString &ATagName, const QString &ANamespace)
 {
-  return FChashElem.elementsByTagNameNS(ANamespace,ATagName).at(0).toElement();
+  if (AStreamJid.isValid() && !ATagName.isEmpty() && !ANamespace.isEmpty())
+  {
+    Stanza stanza("iq");
+    stanza.setType("set").setId(FStanzaProcessor->newId());
+    QDomElement elem = stanza.addElement("query",NS_JABBER_PRIVATE);
+    elem = elem.appendChild(stanza.createElement(ATagName,ANamespace)).toElement();
+    if (FStanzaProcessor->sendIqStanza(this,AStreamJid,stanza,PRIVATE_STORAGE_TIMEOUT))
+    {
+      QDomElement dataElem = getData(AStreamJid,ATagName,ANamespace);
+      if (dataElem.isNull())
+        dataElem = insertElement(AStreamJid,elem); 
+      FRemoveRequests.insert(stanza.id(),dataElem);
+      return stanza.id();
+    }
+  }
+  return QString();
 }
 
-void PrivateStorage::emitCashedRequests()
+QDomElement PrivateStorage::getStreamElement(const Jid &AStreamJid)
 {
-  QList<int> rIds = FCashedRequests;
-  FCashedRequests.clear();
-  foreach (int rId, rIds)
-    emit requestCompleted(rId);
+  if (!FStreamElements.contains(AStreamJid))
+  {
+    QDomElement elem = FStorage.appendChild(FStorage.createElement("stream")).toElement();
+    FStreamElements.insert(AStreamJid,elem);
+  }
+  return FStreamElements.value(AStreamJid);
+}
+
+void PrivateStorage::removeStreamElement(const Jid &AStreamJid)
+{
+  FStreamElements.remove(AStreamJid);
+}
+
+QDomElement PrivateStorage::insertElement(const Jid &AStreamJid, const QDomElement &AElement)
+{
+  removeElement(AStreamJid,AElement.tagName(),AElement.namespaceURI());
+  QDomElement streamElem = getStreamElement(AStreamJid);
+  return streamElem.appendChild(AElement.cloneNode(true)).toElement();
+}
+
+void PrivateStorage::removeElement(const Jid &AStreamJid, const QString &ATagName, const QString &ANamespace)
+{
+  if (FStreamElements.contains(AStreamJid))
+    FStreamElements[AStreamJid].removeChild(getData(AStreamJid,ATagName,ANamespace));
+}
+
+void PrivateStorage::onStreamOpened(IXmppStream *AXmppStream)
+{
+  emit storageOpened(AXmppStream->jid());
+}
+
+void PrivateStorage::onStreamClosed(IXmppStream *AXmppStream)
+{
+  emit storageClosed(AXmppStream->jid());
+  removeStreamElement(AXmppStream->jid());
 }
 
 Q_EXPORT_PLUGIN2(PrivateStoragePlugin, PrivateStorage)
--- Vacuum/trunk/src/plugins/PrivateStorage/privatestorage.h
+++ Vacuum/trunk/src/plugins/PrivateStorage/privatestorage.h
@@ -5,6 +5,7 @@
 #include "../../interfaces/ipluginmanager.h"
 #include "../../interfaces/iprivatestorage.h"
 #include "../../interfaces/istanzaprocessor.h"
+#include "../../interfaces/ixmppstreams.h"
 #include "../../utils/stanza.h"
 
 class PrivateStorage :
@@ -15,13 +16,10 @@ class PrivateStorage :
 {
   Q_OBJECT;
   Q_INTERFACES(IPlugin IPrivateStorage IIqStanzaOwner);
-
 public:
   PrivateStorage();
   ~PrivateStorage();
-
   virtual QObject *instance() { return this; }
-
   //IPlugin
   virtual QUuid pluginUuid() const { return PRIVATESTORAGE_UUID; }
   virtual void pluginInfo(PluginInfo *APluginInfo);
@@ -29,31 +27,38 @@ public:
   virtual bool initObjects() { return true; }
   virtual bool initSettings() { return true; }
   virtual bool startPlugin() { return true; }
-
   //IIqStanzaOwner
   virtual void iqStanza(const Jid &AStreamJid, const Stanza &AStanza);
   virtual void iqStanzaTimeOut(const QString &AId);
-
   //IPrivateStorage
-  virtual int saveElement(const Jid &AStreamJid, const QDomElement &AElement);
-  virtual int loadElement(const Jid &AStreamJid, const QString &ATagName, const QString &ANamespace, 
-    bool ACheckCash = true);
-  virtual int removeElement(const Jid &AStreamJid, const QString &ATagName, const QString &ANamespace);
-  virtual QDomElement getFromCash(const QString &ATagName, const QString &ANamespace) const;
+  virtual bool hasData(const Jid &AStreamJid, const QString &ATagName, const QString &ANamespace) const;
+  virtual QDomElement getData(const Jid &AStreamJid, const QString &ATagName, const QString &ANamespace) const;
+  virtual QString saveData(const Jid &AStreamJid, const QDomElement &AElement);
+  virtual QString loadData(const Jid &AStreamJid, const QString &ATagName, const QString &ANamespace);
+  virtual QString removeData(const Jid &AStreamJid, const QString &ATagName, const QString &ANamespace);
 signals:
-  virtual void requestCompleted(int ARequestId);
-  virtual void requestError(int ARequestId);
-  virtual void requestTimeout(int ARequestId);
+  virtual void storageOpened(const Jid &AStreamJid);
+  virtual void dataSaved(const QString &AId, const Jid &AStreamJid, const QDomElement &AElement);
+  virtual void dataLoaded(const QString &AId, const Jid &AStreamJid, const QDomElement &AElement);
+  virtual void dataRemoved(const QString &AId, const Jid &AStreamJid, const QDomElement &AElement);
+  virtual void dataError(const QString &AId, const QString &AError);
+  virtual void storageClosed(const Jid &AStreamJid);
+protected:
+  QDomElement getStreamElement(const Jid &AStreamJid);
+  void removeStreamElement(const Jid &AStreamJid);
+  QDomElement insertElement(const Jid &AStreamJid, const QDomElement &AElement);
+  void removeElement(const Jid &AStreamJid, const QString &ATagName, const QString &ANamespace);
 protected slots:
-  void emitCashedRequests();
+  void onStreamOpened(IXmppStream *AXmppStream);
+  void onStreamClosed(IXmppStream *AXmppStream);
 private:
   IStanzaProcessor *FStanzaProcessor;
 private:
-  int FRequestId;
-  QDomDocument FCash;
-  QDomElement FChashElem;
-  QList<int> FCashedRequests;
-  QHash<QString,int> FIq2Request;
+  QDomDocument FStorage;
+  QHash<Jid, QDomElement> FStreamElements;
+  QHash<QString, QDomElement> FSaveRequests;
+  QHash<QString, QDomElement> FLoadRequests;
+  QHash<QString, QDomElement> FRemoveRequests;
 };
 
 #endif // PRIVATESTORAGE_H
