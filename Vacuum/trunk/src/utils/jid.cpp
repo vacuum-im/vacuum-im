6eea3fbd9f8724320e14d5ecb91f5e79ba93a00a
potapov.s.a <potapov.s.a@8ee3b2d0-dc07-11de-995a-ab19b33c1a40>
2008-03-04 12:37:57 +0000

    Исправлен алгоритм "Jid Escaping", теперь отдельный символ '\' не кодируется.

--- Vacuum/trunk/src/utils/jid.cpp
+++ Vacuum/trunk/src/utils/jid.cpp
@@ -1,7 +1,7 @@
 #include "jid.h"
 
-QList<QChar> Jid::escChars = QList<QChar>()       << 0x20 << 0x22 << 0x26 << 0x27 << 0x2f << 0x3a << 0x3c << 0x3e << 0x40 << 0x5c;
-QList<QString> Jid::escStrings = QList<QString>() <<"\\20"<<"\\22"<<"\\26"<<"\\27"<<"\\2f"<<"\\3a"<<"\\3c"<<"\\3e"<<"\\40"<<"\\5c";
+QList<QChar> Jid::escChars = QList<QChar>()       << 0x20 << 0x22 << 0x26 << 0x27 << 0x2f << 0x3a << 0x3c << 0x3e << 0x40; // << 0x5c;
+QList<QString> Jid::escStrings = QList<QString>() <<"\\20"<<"\\22"<<"\\26"<<"\\27"<<"\\2f"<<"\\3a"<<"\\3c"<<"\\3e"<<"\\40"; //<<"\\5c";
 
 Jid::Jid(const char *AJidStr)
 {
