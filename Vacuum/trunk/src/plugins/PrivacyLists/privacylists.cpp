adacbb4e252512d1b5ec4ba24714a506da8e1bb6
potapov.s.a <potapov.s.a@8ee3b2d0-dc07-11de-995a-ab19b33c1a40>
2008-05-26 12:22:38 +0000

    Исправлена строка маски станз относящихся к PrivacyLists.

--- Vacuum/trunk/src/plugins/PrivacyLists/privacylists.cpp
+++ Vacuum/trunk/src/plugins/PrivacyLists/privacylists.cpp
@@ -1,7 +1,7 @@
 #include "privacylists.h"
 
-#define SHC_PRIVACY         "/iq[@type='set']/[@xmlns="NS_JABBER_PRIVACY"]"
-#define SHC_ROSTER          "/iq/query[@xmlns='" NS_JABBER_ROSTER "']"
+#define SHC_PRIVACY         "/iq[@type='set']/query[@xmlns='"NS_JABBER_PRIVACY"']"
+#define SHC_ROSTER          "/iq/query[@xmlns='"NS_JABBER_ROSTER"']"
 
 #define PRIVACY_TIMEOUT     60000
 #define AUTO_LISTS_TIMEOUT  2000
