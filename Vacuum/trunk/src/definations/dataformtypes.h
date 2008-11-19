b2cfccc50eebe1330002cd8955eed918b734cf2c
potapov.s.a <potapov.s.a@8ee3b2d0-dc07-11de-995a-ab19b33c1a40>
2008-11-19 14:05:41 +0000

    Добавлена пустая строка и несколько комментариев в файл dataformtypes.h

--- Vacuum/trunk/src/definations/dataformtypes.h
+++ Vacuum/trunk/src/definations/dataformtypes.h
@@ -1,17 +1,22 @@
 #ifndef DATAFORMTYPES_H
 #define DATAFORMTYPES_H
 
+//ClientInfo
 #define DATA_FORM_SOFTWAREINFO                "urn:xmpp:dataforms:softwareinfo"
 
+//Registration
 #define DATA_FORM_REGISTER                    "jabber:iq:register"
 
+//MultiUserChat
 #define DATA_FORM_MUC_REGISTER                "http://jabber.org/protocol/muc#register"
 #define DATA_FORM_MUC_ROOMCONFIG              "http://jabber.org/protocol/muc#roomconfig"
 #define DATA_FORM_MUC_REQUEST                 "http://jabber.org/protocol/muc#request"
 #define DATA_FORM_MUC_ROOM_INFO               "http://jabber.org/protocol/muc#roominfo"
 
+//JabberSearch
 #define DATA_FORM_SEARCH                      "jabber:iq:search"
 
+//SessionNegotiation
 #define DATA_FORM_SESSION_NEGOTIATION         "urn:xmpp:ssn"
 
 #endif
