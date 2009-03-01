0ab1baa179a33c2ef38a5c0b8a39abd45a30885d
Sergey Potapov <potapov.s.a@gmail.com>
2009-03-01 10:53:01 +0000

    Обновлена локализация на русский язык плагина StatusChanger.

--- src/translations/ru_RU/statuschanger.ts
+++ src/translations/ru_RU/statuschanger.ts
@@ -23,70 +23,70 @@
     <message>
         <location filename="../../plugins/statuschanger/editstatusdialog.cpp" line="147"/>
         <source>Icon</source>
-        <translation>Иконка</translation>
+        <translation type="obsolete">Иконка</translation>
     </message>
     <message>
-        <location filename="../../plugins/statuschanger/editstatusdialog.cpp" line="147"/>
+        <location filename="../../plugins/statuschanger/editstatusdialog.cpp" line="148"/>
         <source>Name</source>
         <translation>Имя</translation>
     </message>
     <message>
-        <location filename="../../plugins/statuschanger/editstatusdialog.cpp" line="147"/>
+        <location filename="../../plugins/statuschanger/editstatusdialog.cpp" line="148"/>
         <source>Show</source>
         <translation>Статус</translation>
     </message>
     <message>
-        <location filename="../../plugins/statuschanger/editstatusdialog.cpp" line="147"/>
+        <location filename="../../plugins/statuschanger/editstatusdialog.cpp" line="148"/>
         <source>Message</source>
         <translation>Сообщение</translation>
     </message>
     <message>
-        <location filename="../../plugins/statuschanger/editstatusdialog.cpp" line="147"/>
+        <location filename="../../plugins/statuschanger/editstatusdialog.cpp" line="148"/>
         <source>Priority</source>
         <translation>Приоритет</translation>
     </message>
     <message>
-        <location filename="../../plugins/statuschanger/editstatusdialog.cpp" line="252"/>
+        <location filename="../../plugins/statuschanger/editstatusdialog.cpp" line="242"/>
         <source>Enter status name</source>
         <translation>Введите имя статуса</translation>
     </message>
     <message>
-        <location filename="../../plugins/statuschanger/editstatusdialog.cpp" line="252"/>
+        <location filename="../../plugins/statuschanger/editstatusdialog.cpp" line="242"/>
         <source>Status name:</source>
         <translation>Имя статуса:</translation>
     </message>
     <message>
-        <location filename="../../plugins/statuschanger/editstatusdialog.cpp" line="296"/>
+        <location filename="../../plugins/statuschanger/editstatusdialog.cpp" line="280"/>
         <source>Wrong status name</source>
         <translation>Неверное имя статуса</translation>
     </message>
     <message>
-        <location filename="../../plugins/statuschanger/editstatusdialog.cpp" line="296"/>
+        <location filename="../../plugins/statuschanger/editstatusdialog.cpp" line="280"/>
         <source>Status with name &apos;&lt;b&gt;%1&lt;/b&gt;&apos; allready exists</source>
         <translation>Статус с именем &apos;&lt;b&gt;%1&lt;/b&gt;&apos; уже присутствует</translation>
     </message>
     <message>
-        <location filename="../../plugins/statuschanger/editstatusdialog.cpp" line="319"/>
+        <location filename="../../plugins/statuschanger/editstatusdialog.cpp" line="303"/>
         <source>Can&apos;t delete status</source>
         <translation>Невозможно удалить статус</translation>
     </message>
     <message>
-        <location filename="../../plugins/statuschanger/editstatusdialog.cpp" line="315"/>
+        <location filename="../../plugins/statuschanger/editstatusdialog.cpp" line="299"/>
         <source>You can not delete standard statuses.</source>
         <translation>Вы не можете удалить стандартные статусы.</translation>
     </message>
     <message>
-        <location filename="../../plugins/statuschanger/editstatusdialog.cpp" line="319"/>
+        <location filename="../../plugins/statuschanger/editstatusdialog.cpp" line="303"/>
         <source>You can not delete active statuses.</source>
         <translation>Вы не можете удалить активные статусы.</translation>
     </message>
     <message>
-        <location filename="../../plugins/statuschanger/editstatusdialog.cpp" line="323"/>
+        <location filename="../../plugins/statuschanger/editstatusdialog.cpp" line="307"/>
         <source>Delete status</source>
         <translation>Удалить статус</translation>
     </message>
     <message>
-        <location filename="../../plugins/statuschanger/editstatusdialog.cpp" line="324"/>
+        <location filename="../../plugins/statuschanger/editstatusdialog.cpp" line="308"/>
         <source>You are assured that wish to remove a status &apos;&lt;b&gt;%1&lt;/b&gt;&apos;?</source>
         <translation>Вы уверены, что хотите удалить статус &apos;&lt;b&gt;%1&lt;/b&gt;&apos;?</translation>
     </message>
@@ -112,102 +112,102 @@
 <context>
     <name>StatusChanger</name>
     <message>
-        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="54"/>
+        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="55"/>
         <source>Managing and change status</source>
         <translation>Управление и изменение статусов</translation>
     </message>
     <message>
-        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="56"/>
+        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="57"/>
         <source>Status Changer</source>
         <translation>Менеджер статусов</translation>
     </message>
     <message>
-        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="160"/>
+        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="161"/>
         <source>Edit presence statuses</source>
         <translation>Редактировать статусы</translation>
     </message>
     <message>
-        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="202"/>
+        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="203"/>
         <source>Connection errors</source>
         <translation>Ошибки подключения</translation>
     </message>
     <message>
-        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="484"/>
+        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="454"/>
         <source>Offline</source>
         <translation>Не в сети</translation>
     </message>
     <message>
-        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="510"/>
+        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="480"/>
         <source>Online</source>
         <translation>В сети</translation>
     </message>
     <message>
-        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="488"/>
+        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="458"/>
         <source>Chat</source>
         <translation>Готов общаться</translation>
     </message>
     <message>
-        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="490"/>
+        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="460"/>
         <source>Away</source>
         <translation>Ушёл</translation>
     </message>
     <message>
-        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="492"/>
+        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="462"/>
         <source>Extended Away</source>
         <translation>Ушёл надолго</translation>
     </message>
     <message>
-        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="537"/>
+        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="507"/>
         <source>Do not disturb</source>
         <translation>Не беспокоить</translation>
     </message>
     <message>
-        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="496"/>
+        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="466"/>
         <source>Invisible</source>
         <translation>Невидимый</translation>
     </message>
     <message>
-        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="573"/>
+        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="543"/>
         <source>Error</source>
         <translation>Ошибка</translation>
     </message>
     <message>
-        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="500"/>
+        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="470"/>
         <source>Unknown Status</source>
         <translation>Неизвестный статус</translation>
     </message>
     <message>
-        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="519"/>
+        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="489"/>
         <source>Free for chat</source>
         <translation>Поболтаем?</translation>
     </message>
     <message>
-        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="528"/>
+        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="498"/>
         <source>I`am away from my desk</source>
         <translation>Отошел</translation>
     </message>
     <message>
-        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="546"/>
+        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="516"/>
         <source>Not available</source>
         <translation>Не доступен</translation>
     </message>
     <message>
-        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="564"/>
+        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="534"/>
         <source>Disconnected</source>
         <translation>Отключен</translation>
     </message>
     <message>
-        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="782"/>
+        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="736"/>
         <source>%1 - %2</source>
         <translation>%1 - %2</translation>
     </message>
     <message>
-        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="891"/>
+        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="842"/>
         <source>Connection error</source>
         <translation>Ошибка подключения</translation>
     </message>
     <message>
-        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="1026"/>
+        <location filename="../../plugins/statuschanger/statuschanger.cpp" line="977"/>
         <source>Status</source>
         <translation>Статус</translation>
     </message>
