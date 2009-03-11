47a616274e0c8969d48d292efc14e8ad992c605a
Sergey Potapov <potapov.s.a@gmail.com>
2009-03-11 21:42:09 +0000

    Использование QNetworkProxy вместо собственной реализации.

--- src/plugins/defaultconnection/defaultconnection.cpp
+++ src/plugins/defaultconnection/defaultconnection.cpp
@@ -1,6 +1,7 @@
 #include "defaultconnection.h"
 
 #include <qendian.h>
+#include <QNetworkProxy>
 
 #define CONNECT_TIMEOUT       30000
 #define DISCONNECT_TIMEOUT    5000
@@ -11,7 +12,6 @@ DefaultConnection::DefaultConnection(IConnectionPlugin *APlugin, QObject *AParen
   FConnected = false;
   FDisconnect = true; 
   FDisconnected = true;
-  FProxyState = ProxyUnconnected;
   FSocket.setProtocol(QSsl::AnyProtocol);
 
   connect(&FSocket, SIGNAL(connected()), SLOT(onSocketConnected()));
@@ -48,29 +48,38 @@ void DefaultConnection::connectToHost()
   if (FSocket.state() == QAbstractSocket::UnconnectedState)
   {
     emit aboutToConnect();
-    FHost = option(IDefaultConnection::CO_HOST).toString();
-    FPort = option(IDefaultConnection::CO_PORT).toInt();
+
+    QString host  = option(IDefaultConnection::CO_HOST).toString();
+    quint16 port = option(IDefaultConnection::CO_PORT).toInt();
     FUseSSL = option(IDefaultConnection::CO_USE_SSL).toBool();
     FIgnoreSSLErrors = option(IDefaultConnection::CO_IGNORE_SSL_ERRORS).toBool();
-    FProxyType = option(IDefaultConnection::CO_PROXY_TYPE).toInt();
-    FProxyHost = option(IDefaultConnection::CO_PROXY_HOST).toString();
-    FProxyPort = option(IDefaultConnection::CO_PROXY_PORT).toInt();
-    FProxyUser = option(IDefaultConnection::CO_PROXY_USER_NAME).toString();
-    FProxyPassword = option(IDefaultConnection::CO_PROXY_PASSWORD).toString();
 
-    if (FProxyType == 0)
+    QNetworkProxy proxy;
+    switch (option(IDefaultConnection::CO_PROXY_TYPE).toInt())
     {
-      FProxyState = ProxyReady;
-      if (FUseSSL)
-        FSocket.connectToHostEncrypted(FHost,FPort);
-      else
-        FSocket.connectToHost(FHost, FPort);
+    case PT_NO_PROXY: 
+      proxy.setType(QNetworkProxy::NoProxy);
+      break;
+    case PT_SOCKET5_PROXY: 
+      proxy.setType(QNetworkProxy::Socks5Proxy);
+      break;
+    case PT_HTTP_PROXY: 
+      proxy.setType(QNetworkProxy::HttpProxy);
+      break;
+    default:
+      proxy.setType(QNetworkProxy::DefaultProxy);
     }
-    else 
-    {
-      FProxyState = ProxyUnconnected;
-      FSocket.connectToHost(FProxyHost, FProxyPort);
-    }
+    proxy.setHostName(option(IDefaultConnection::CO_PROXY_HOST).toString());
+    proxy.setPort(option(IDefaultConnection::CO_PROXY_PORT).toInt());
+    proxy.setUser(option(IDefaultConnection::CO_PROXY_USER_NAME).toString());
+    proxy.setPassword(option(IDefaultConnection::CO_PROXY_PASSWORD).toString());
+    FSocket.setProxy(proxy);
+
+    if (FUseSSL)
+      FSocket.connectToHostEncrypted(host,port);
+    else
+      FSocket.connectToHost(host, port);
+
     FConnectTimer.start(CONNECT_TIMEOUT);
   }
   else
@@ -154,127 +163,6 @@ QList<QSslError> DefaultConnection::sslErrors() const
   return FSocket.sslErrors();
 }
 
-void DefaultConnection::proxyConnection()
-{
-  switch (FProxyType)
-  {
-  case 1: socket5Connection();break;
-  case 2: httpsConnection();break;
-  }
-}
-
-void DefaultConnection::socket5Connection()
-{
-  if (FProxyState == ProxyUnconnected)
-  {
-    QByteArray buf;
-    buf += 5;
-    buf += 1;
-    if (FProxyUser.isEmpty())
-      buf +='\0';
-    else 
-      buf += 2;
-    FProxyState = ProxyAuthType;
-    write(buf); 
-  }
-  else if (FProxyState == ProxyAuthType)
-  {
-    QByteArray buf = read(FSocket.bytesAvailable());
-    if (buf[2] == '\2') //Authentication process
-    {
-      buf.clear();
-      buf += 1;       //version of subnegotiation
-      buf += (const char)FProxyUser.toUtf8().size();
-      buf += FProxyUser.toUtf8();
-      buf += (const char)FProxyPassword.toUtf8().size();
-      buf += FProxyPassword.toUtf8(); 
-      FProxyState = ProxyAuthResult;
-      write(buf);
-    }
-    else
-      FProxyState = ProxyConnect;
-  }
-  else if (FProxyState == ProxyAuthResult)
-  {
-    QByteArray buf = read(FSocket.bytesAvailable());
-    if (buf[1] != '\0')
-      connectionError(tr("Socket5 authentication error"));
-    else
-      FProxyState = ProxyConnect;
-  }
-
-  if (FProxyState == ProxyConnect)
-  {
-    QByteArray buf;
-    buf += 5;
-    buf += 1;
-    buf += '\0';
-    buf += 3;
-    buf += (const char)FHost.toAscii().size();
-    buf += FHost.toAscii();
-    quint16 port = qToBigEndian<quint16>(FPort);
-    buf += QByteArray((const char *)&port,2);
-    FProxyState = ProxyConnectResult;
-    write(buf);
-  }
-  else if (FProxyState == ProxyConnectResult)
-  {
-    QByteArray buf = read(FSocket.bytesAvailable());
-    if (buf[1] != '\0')
-    {
-      QString err;
-      switch (buf[1])
-      {
-        case 1: err = tr("General SOCKS server failure.");break;
-        case 2: err = tr("Connection not allowed by rule set.");break;
-        case 3: err = tr("Network unreachable.");break;
-        case 4: err = tr("Host unreachable.");break;
-        case 5: err = tr("Connection refused.");break;
-        case 6: err = tr("TTL expired.");break;
-        case 7: err = tr("Command not supported.");break;
-        case 8: err = tr("Address type not supported.");break;
-        default: err = tr("Unknown socks error.");break;
-      }
-      connectionError(err);
-    }
-    else
-      proxyReady();
-  }
-}
-
-void DefaultConnection::httpsConnection()
-{
-  if (FProxyState == ProxyUnconnected)
-  {
-    QByteArray buf = "CONNECT " + FHost.trimmed().toAscii()+":"+QString("%1").arg(FPort).toAscii()+" HTTP/1.1\r\n";
-    if (!FProxyUser.isEmpty())
-      buf += "Proxy-Authorization: Basic "+FProxyUser.toUtf8()+":"+FProxyPassword.toUtf8()+"\r\n";
-    buf += "Connection: Keep-Alive\r\n";
-    buf += "Proxy-Connection: Keep-Alive\r\n";
-    buf += "Host: "+FHost.toAscii()+":"+QString("%1").arg(FPort).toAscii()+"\r\n";  
-    buf += "\r\n";
-    FProxyState = ProxyConnectResult;
-    write(buf);
-  }
-  else if (FProxyState == ProxyConnectResult)
-  {
-    QList<QByteArray> result = read(FSocket.bytesAvailable()).split(' ');
-    if (result[1].toInt() != 200)
-      connectionError(result[2]);
-    else
-      proxyReady();
-  }
-}
-
-void DefaultConnection::proxyReady()
-{
-  FProxyState = ProxyReady;
-  if (FUseSSL)
-    FSocket.startClientEncryption();
-  else
-    connectionReady();
-}
-
 void DefaultConnection::connectionReady()
 {
   FConnectTimer.stop();
@@ -290,9 +178,7 @@ void DefaultConnection::connectionError(const QString &AError)
 void DefaultConnection::onSocketConnected()
 {
   FConnected = true;
-  if (FProxyState != ProxyReady)
-    proxyConnection();
-  else if (!FUseSSL)
+  if (!FUseSSL)
     connectionReady();
 }
 
@@ -305,10 +191,7 @@ void DefaultConnection::onSocketEncrypted()
 
 void DefaultConnection::onSocketReadyRead()
 {
-  if (FProxyState != ProxyReady)
-    proxyConnection();
-  else
-    emit readyRead(FSocket.bytesAvailable()); 
+  emit readyRead(FSocket.bytesAvailable()); 
 }
 
 void DefaultConnection::onSocketSSLErrors(const QList<QSslError> &AErrors)
