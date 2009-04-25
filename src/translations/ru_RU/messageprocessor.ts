f8ef3c93567b60454d951dca5de3fb0d3758747c
Sergey Potapov <potapov.s.a@gmail.com>
2009-04-25 06:56:32 +0000

    Поправлена локализация плагина IMessageProcessor

--- src/translations/ru_RU/messageprocessor.ts
+++ src/translations/ru_RU/messageprocessor.ts
@@ -1,15 +1,15 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.0" language="ru_RU" sourcelanguage="en">
 <context>
     <name>MessageProcessor</name>
     <message>
         <source>Managing message stanzas</source>
-        <translation type="unfinished"></translation>
+        <translation>Управляет обработкой сообщений</translation>
     </message>
     <message>
         <source>Message Processor</source>
-        <translation type="unfinished"></translation>
+        <translation>Модуль управления сообщениями</translation>
     </message>
 </context>
 </TS>
