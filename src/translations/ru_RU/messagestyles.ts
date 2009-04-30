04cc8cf4bd611998b35b5ee3430fb0ebc5718a2e
Sergey Potapov <potapov.s.a@gmail.com>
2009-04-30 19:14:43 +0000

    Обновлена русская локализация

--- src/translations/ru_RU/messagestyles.ts
+++ src/translations/ru_RU/messagestyles.ts
@@ -1,38 +1,38 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.0" language="ru_RU" sourcelanguage="en">
 <context>
     <name>MessageStyle</name>
     <message>
         <source>hh:mm</source>
-        <translation type="unfinished"></translation>
+        <translation>hh:mm</translation>
     </message>
     <message>
         <source>hh:mm:ss</source>
-        <translation type="unfinished"></translation>
+        <translation>hh:mm:ss</translation>
     </message>
 </context>
 <context>
     <name>MessageStyles</name>
     <message>
         <source>Managing message styles</source>
-        <translation type="unfinished"></translation>
+        <translation>Управление стилями сообщений</translation>
     </message>
     <message>
         <source>Message Styles</source>
-        <translation type="unfinished"></translation>
+        <translation>Стили сообщений</translation>
     </message>
     <message>
         <source>d MMM yyyy hh:mm</source>
-        <translation type="unfinished"></translation>
+        <translation>d MMM yyyy hh:mm</translation>
     </message>
     <message>
         <source>d MMM hh:mm</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>hh:mm:ss</source>
-        <translation type="unfinished"></translation>
+        <translation>hh:mm:ss</translation>
     </message>
 </context>
 </TS>
