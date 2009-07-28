3e747f0f6f6e902fe1513cb6ccda25a74a3d4b64
Sergey Potapov <potapov.s.a@gmail.com>
2009-07-28 11:54:30 +0000

    Забытое обновление локализации

--- src/translations/ru_RU/accountmanager.ts
+++ src/translations/ru_RU/accountmanager.ts
@@ -2,44 +2,6 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="ru_RU" sourcelanguage="en">
 <context>
-    <name>AccountManage</name>
-    <message>
-        <source>Name</source>
-        <translation>Имя</translation>
-    </message>
-    <message>
-        <source>Jabber ID</source>
-        <translation>Jabber ID</translation>
-    </message>
-    <message>
-        <source>Enter account name</source>
-        <translation>Введите имя аккаунта</translation>
-    </message>
-    <message>
-        <source>Account name:</source>
-        <translation>Имя аккаунта:</translation>
-    </message>
-    <message>
-        <source>Confirm removal of an account.</source>
-        <translation>Подтвердите удаление аккаунта.</translation>
-    </message>
-    <message>
-        <source>You are assured that wish to remove an account &lt;b&gt;%1&lt;/b&gt;?&lt;br&gt;All settings will be lost.</source>
-        <translation>Вы уверены, что хотите удалить аккаунт &lt;b&gt;%1&lt;/b&gt;?&lt;br&gt;Все настройки будут потеряны.</translation>
-    </message>
-</context>
-<context>
-    <name>AccountManageClass</name>
-    <message>
-        <source>Add</source>
-        <translation>Добавить</translation>
-    </message>
-    <message>
-        <source>Remove</source>
-        <translation>Удалить</translation>
-    </message>
-</context>
-<context>
     <name>AccountManager</name>
     <message>
         <source>Creating and removing accounts</source>
@@ -50,28 +12,23 @@
         <translation>Аккаунты</translation>
     </message>
     <message>
-        <source>&lt;Empty&gt;</source>
-        <translation>&lt;Пусто&gt;</translation>
-    </message>
-    <message>
-        <source>Account details and connection options</source>
-        <translation>Параметры аккаунта и настройки соединения</translation>
-    </message>
-    <message>
-        <source>Not valid account</source>
-        <translation>Ошибка в аккаунте</translation>
-    </message>
-    <message>
-        <source>Account %1 is not valid, change its Jabber ID</source>
-        <translation>Аккаунт %1 неправильный, измените его Jabber ID</translation>
-    </message>
-    <message>
         <source>Modify account</source>
         <translation>Изменить аккаунт</translation>
     </message>
     <message>
         <source>Account manager</source>
         <translation>Менеджер аккаунтов</translation>
+    </message>
+    <message>
+        <source>Account options</source>
+        <translation>Настройки аккаунта</translation>
+    </message>
+</context>
+<context>
+    <name>AccountOptions</name>
+    <message>
+        <source>New Account</source>
+        <translation>Новый Аккаунт</translation>
     </message>
 </context>
 <context>
@@ -103,6 +60,52 @@
     <message>
         <source>Example: balcony; orchard</source>
         <translation>Пример:  балкон; сад</translation>
+    </message>
+</context>
+<context>
+    <name>AccountsOptions</name>
+    <message>
+        <source>Name</source>
+        <translation>Имя</translation>
+    </message>
+    <message>
+        <source>Jabber ID</source>
+        <translation>Jabber ID</translation>
+    </message>
+    <message>
+        <source>Not valid account</source>
+        <translation>Ошибка в аккаунте</translation>
+    </message>
+    <message>
+        <source>Account %1 is not valid, change its Jabber ID</source>
+        <translation>Аккаунт %1 неправильный, измените его Jabber ID</translation>
+    </message>
+    <message>
+        <source>Enter account name</source>
+        <translation>Введите имя аккаунта</translation>
+    </message>
+    <message>
+        <source>Account name:</source>
+        <translation>Имя аккаунта:</translation>
+    </message>
+    <message>
+        <source>Confirm removal of an account</source>
+        <translation>Подтвердите удаление аккаунта</translation>
+    </message>
+    <message>
+        <source>You are assured that wish to remove an account &lt;b&gt;%1&lt;/b&gt;?&lt;br&gt;All settings will be lost.</source>
+        <translation>Вы уверены, что хотите удалить аккаунт &lt;b&gt;%1&lt;/b&gt;?&lt;br&gt;Все настройки будут потеряны.</translation>
+    </message>
+</context>
+<context>
+    <name>AccountsOptionsClass</name>
+    <message>
+        <source>Add</source>
+        <translation>Добавить</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>Удалить</translation>
     </message>
 </context>
 </TS>
