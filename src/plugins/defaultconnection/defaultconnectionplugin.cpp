203ef411798cdfe77c975b2f921294e76a416714
Sergey Potapov <potapov.s.a@gmail.com>
2009-08-26 17:32:18 +0000

    В DefaultConnection по умолчанию включено игнорирование ошибок SSL.

--- src/plugins/defaultconnection/defaultconnectionplugin.cpp
+++ src/plugins/defaultconnection/defaultconnectionplugin.cpp
@@ -86,51 +86,61 @@ void DefaultConnectionPlugin::destroyConnection(IConnection *AConnection)
 
 void DefaultConnectionPlugin::loadSettings(IConnection *AConnection, const QString &ASettingsNS)
 {
-  AConnection->setOption(IDefaultConnection::CO_HOST,FSettings->valueNS(SVN_CONNECTION_HOST,ASettingsNS,QString()));
-  AConnection->setOption(IDefaultConnection::CO_PORT,FSettings->valueNS(SVN_CONNECTION_PORT,ASettingsNS,5222));
-  AConnection->setOption(IDefaultConnection::CO_USE_SSL,FSettings->valueNS(SVN_CONNECTION_USE_SSL,ASettingsNS,false));
-  AConnection->setOption(IDefaultConnection::CO_IGNORE_SSL_ERRORS,FSettings->valueNS(SVN_CONNECTION_IGNORE_SSLERROR,ASettingsNS,false));
-  AConnection->setOption(IDefaultConnection::CO_PROXY_TYPE,FSettings->valueNS(SVN_CONNECTION_PROXY_TYPE,ASettingsNS,0));
-  AConnection->setOption(IDefaultConnection::CO_PROXY_HOST,FSettings->valueNS(SVN_CONNECTION_PROXY_HOST,ASettingsNS,QString()));
-  AConnection->setOption(IDefaultConnection::CO_PROXY_PORT,FSettings->valueNS(SVN_CONNECTION_PROXY_PORT,ASettingsNS,0));
-  AConnection->setOption(IDefaultConnection::CO_PROXY_USER_NAME,FSettings->valueNS(SVN_CONNECTION_PROXY_USER,ASettingsNS,QString()));
-  AConnection->setOption(IDefaultConnection::CO_PROXY_PASSWORD, 
-    FSettings->decript(FSettings->valueNS(SVN_CONNECTION_PROXY_PSWD,ASettingsNS,QString()).toByteArray(),ASettingsNS.toUtf8()));
-  emit connectionUpdated(AConnection,ASettingsNS);
+  if (FSettings)
+  {
+    AConnection->setOption(IDefaultConnection::CO_HOST,FSettings->valueNS(SVN_CONNECTION_HOST,ASettingsNS,QString()));
+    AConnection->setOption(IDefaultConnection::CO_PORT,FSettings->valueNS(SVN_CONNECTION_PORT,ASettingsNS,5222));
+    AConnection->setOption(IDefaultConnection::CO_USE_SSL,FSettings->valueNS(SVN_CONNECTION_USE_SSL,ASettingsNS,false));
+    AConnection->setOption(IDefaultConnection::CO_IGNORE_SSL_ERRORS,FSettings->valueNS(SVN_CONNECTION_IGNORE_SSLERROR,ASettingsNS,true));
+    AConnection->setOption(IDefaultConnection::CO_PROXY_TYPE,FSettings->valueNS(SVN_CONNECTION_PROXY_TYPE,ASettingsNS,0));
+    AConnection->setOption(IDefaultConnection::CO_PROXY_HOST,FSettings->valueNS(SVN_CONNECTION_PROXY_HOST,ASettingsNS,QString()));
+    AConnection->setOption(IDefaultConnection::CO_PROXY_PORT,FSettings->valueNS(SVN_CONNECTION_PROXY_PORT,ASettingsNS,0));
+    AConnection->setOption(IDefaultConnection::CO_PROXY_USER_NAME,FSettings->valueNS(SVN_CONNECTION_PROXY_USER,ASettingsNS,QString()));
+    AConnection->setOption(IDefaultConnection::CO_PROXY_PASSWORD, 
+      FSettings->decript(FSettings->valueNS(SVN_CONNECTION_PROXY_PSWD,ASettingsNS,QString()).toByteArray(),ASettingsNS.toUtf8()));
+    emit connectionUpdated(AConnection,ASettingsNS);
+  }
 }
 
 void DefaultConnectionPlugin::saveSettings(IConnection *AConnection, const QString &ASettingsNS)
 {
-  FSettings->setValueNS(SVN_CONNECTION_HOST,ASettingsNS,AConnection->option(IDefaultConnection::CO_HOST));
-  FSettings->setValueNS(SVN_CONNECTION_PORT,ASettingsNS,AConnection->option(IDefaultConnection::CO_PORT));
-  FSettings->setValueNS(SVN_CONNECTION_USE_SSL,ASettingsNS,AConnection->option(IDefaultConnection::CO_USE_SSL));
-  FSettings->setValueNS(SVN_CONNECTION_IGNORE_SSLERROR,ASettingsNS,AConnection->option(IDefaultConnection::CO_IGNORE_SSL_ERRORS));
-  FSettings->setValueNS(SVN_CONNECTION_PROXY_TYPE,ASettingsNS,AConnection->option(IDefaultConnection::CO_PROXY_TYPE));
-  FSettings->setValueNS(SVN_CONNECTION_PROXY_HOST,ASettingsNS,AConnection->option(IDefaultConnection::CO_PROXY_HOST));
-  FSettings->setValueNS(SVN_CONNECTION_PROXY_PORT,ASettingsNS,AConnection->option(IDefaultConnection::CO_PROXY_PORT));
-  FSettings->setValueNS(SVN_CONNECTION_PROXY_USER,ASettingsNS,AConnection->option(IDefaultConnection::CO_PROXY_USER_NAME));
-  FSettings->setValueNS(SVN_CONNECTION_PROXY_PSWD,ASettingsNS,
-    FSettings->encript(AConnection->option(IDefaultConnection::CO_PROXY_PASSWORD).toString(),ASettingsNS.toUtf8()));
+  if (FSettings)
+  {
+    FSettings->setValueNS(SVN_CONNECTION_HOST,ASettingsNS,AConnection->option(IDefaultConnection::CO_HOST));
+    FSettings->setValueNS(SVN_CONNECTION_PORT,ASettingsNS,AConnection->option(IDefaultConnection::CO_PORT));
+    FSettings->setValueNS(SVN_CONNECTION_USE_SSL,ASettingsNS,AConnection->option(IDefaultConnection::CO_USE_SSL));
+    FSettings->setValueNS(SVN_CONNECTION_IGNORE_SSLERROR,ASettingsNS,AConnection->option(IDefaultConnection::CO_IGNORE_SSL_ERRORS));
+    FSettings->setValueNS(SVN_CONNECTION_PROXY_TYPE,ASettingsNS,AConnection->option(IDefaultConnection::CO_PROXY_TYPE));
+    FSettings->setValueNS(SVN_CONNECTION_PROXY_HOST,ASettingsNS,AConnection->option(IDefaultConnection::CO_PROXY_HOST));
+    FSettings->setValueNS(SVN_CONNECTION_PROXY_PORT,ASettingsNS,AConnection->option(IDefaultConnection::CO_PROXY_PORT));
+    FSettings->setValueNS(SVN_CONNECTION_PROXY_USER,ASettingsNS,AConnection->option(IDefaultConnection::CO_PROXY_USER_NAME));
+    FSettings->setValueNS(SVN_CONNECTION_PROXY_PSWD,ASettingsNS,
+      FSettings->encript(AConnection->option(IDefaultConnection::CO_PROXY_PASSWORD).toString(),ASettingsNS.toUtf8()));
+  }
 }
 
 void DefaultConnectionPlugin::deleteSettingsNS(const QString &ASettingsNS)
 {
-  FSettings->deleteNS(ASettingsNS);
+  if (FSettings)
+    FSettings->deleteNS(ASettingsNS);
 }
 
 QWidget *DefaultConnectionPlugin::optionsWidget(const QString &ASettingsNS)
 {
   ConnectionOptionsWidget *widget = new ConnectionOptionsWidget;
-  widget->setHost(FSettings->valueNS(SVN_CONNECTION_HOST,ASettingsNS,QString()).toString());
-  widget->setPort(FSettings->valueNS(SVN_CONNECTION_PORT,ASettingsNS,5222).toInt());
-  widget->setUseSSL(FSettings->valueNS(SVN_CONNECTION_USE_SSL,ASettingsNS,false).toBool());
-  widget->setIgnoreSSLError(FSettings->valueNS(SVN_CONNECTION_IGNORE_SSLERROR,ASettingsNS,false).toBool());
-  widget->setProxyTypes(proxyTypeNames());
-  widget->setProxyType(FSettings->valueNS(SVN_CONNECTION_PROXY_TYPE,ASettingsNS,0).toInt());
-  widget->setProxyHost(FSettings->valueNS(SVN_CONNECTION_PROXY_HOST,ASettingsNS,QString()).toString());
-  widget->setProxyPort(FSettings->valueNS(SVN_CONNECTION_PROXY_PORT,ASettingsNS,0).toInt());
-  widget->setProxyUserName(FSettings->valueNS(SVN_CONNECTION_PROXY_USER,ASettingsNS,QString()).toString());
-  widget->setProxyPassword(FSettings->decript(FSettings->valueNS(SVN_CONNECTION_PROXY_PSWD,ASettingsNS,QString()).toByteArray(),ASettingsNS.toUtf8())); 
+  if (FSettings)
+  {
+    widget->setHost(FSettings->valueNS(SVN_CONNECTION_HOST,ASettingsNS,QString()).toString());
+    widget->setPort(FSettings->valueNS(SVN_CONNECTION_PORT,ASettingsNS,5222).toInt());
+    widget->setUseSSL(FSettings->valueNS(SVN_CONNECTION_USE_SSL,ASettingsNS,false).toBool());
+    widget->setIgnoreSSLError(FSettings->valueNS(SVN_CONNECTION_IGNORE_SSLERROR,ASettingsNS,false).toBool());
+    widget->setProxyTypes(proxyTypeNames());
+    widget->setProxyType(FSettings->valueNS(SVN_CONNECTION_PROXY_TYPE,ASettingsNS,0).toInt());
+    widget->setProxyHost(FSettings->valueNS(SVN_CONNECTION_PROXY_HOST,ASettingsNS,QString()).toString());
+    widget->setProxyPort(FSettings->valueNS(SVN_CONNECTION_PROXY_PORT,ASettingsNS,0).toInt());
+    widget->setProxyUserName(FSettings->valueNS(SVN_CONNECTION_PROXY_USER,ASettingsNS,QString()).toString());
+    widget->setProxyPassword(FSettings->decript(FSettings->valueNS(SVN_CONNECTION_PROXY_PSWD,ASettingsNS,QString()).toByteArray(),ASettingsNS.toUtf8())); 
+  }
   FWidgetsByNS.insert(ASettingsNS,widget);
   return widget;
 }
@@ -138,7 +148,7 @@ QWidget *DefaultConnectionPlugin::optionsWidget(const QString &ASettingsNS)
 void DefaultConnectionPlugin::saveOptions(const QString &ASettingsNS)
 {
   ConnectionOptionsWidget *widget = FWidgetsByNS.value(ASettingsNS,NULL);
-  if (widget)
+  if (FSettings && widget)
   {
     FSettings->setValueNS(SVN_CONNECTION_HOST,ASettingsNS,widget->host());
     FSettings->setValueNS(SVN_CONNECTION_PORT,ASettingsNS,widget->port());
