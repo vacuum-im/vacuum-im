f168b0c176b129e0cbec4c38713b270325e8b636
Sergey Potapov <potapov.s.a@gmail.com>
2010-02-19 20:01:52 +0000

    Исправлена ошибка, из-за которой в окно чата с Adium стилем можно было загрузить произвольную страницу через контекстное меню ссылки.

--- src/plugins/adiummessagestyle/styleviewer.h
+++ src/plugins/adiummessagestyle/styleviewer.h
@@ -2,6 +2,7 @@
 #define STYLEVIEWER_H
 
 #include <QWebView>
+#include <webpage.h>
 
 class StyleViewer : 
   public QWebView
---
+++ src/plugins/adiummessagestyle/webpage.h
@@ -0,0 +1,17 @@
+#ifndef WEBPAGE_H
+#define WEBPAGE_H
+
+#include <QWebPage>
+
+class WebPage : 
+  public QWebPage
+{
+  Q_OBJECT;
+public:
+  WebPage(QObject *AParent = NULL);
+  ~WebPage();
+  // QWebPage
+  virtual void triggerAction(WebAction AAction, bool AChecked = false);
+};
+
+#endif // WEBPAGE_H
