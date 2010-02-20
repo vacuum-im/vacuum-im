a267761c59007ad643dafa4c3d8419d016025705
Sergey Potapov <potapov.s.a@gmail.com>
2010-02-20 09:54:51 +0000

    В настройке автозапуска убрано сбивавшее с толку промежуточное состояние флажка, означавшее наличие автозапуска вакуума из другого пути.

--- src/plugins/settings/miscoptionswidget.cpp
+++ src/plugins/settings/miscoptionswidget.cpp
@@ -9,12 +9,10 @@ MiscOptionsWidget::MiscOptionsWidget(QWidget *AParent) : QWidget(AParent)
 #ifdef Q_WS_WIN
   QSettings reg("HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Run", QSettings::NativeFormat);
   QString startPath = reg.value(CLIENT_NAME, "").toString();
-  if (startPath.isEmpty())
-    ui.chbAutorun->setCheckState(Qt::Unchecked);
-  else if (startPath.toLower() == QDir::toNativeSeparators(QApplication::applicationFilePath()).toLower())
+  if (startPath.toLower() == QDir::toNativeSeparators(QApplication::applicationFilePath()).toLower())
     ui.chbAutorun->setCheckState(Qt::Checked);
   else
-    ui.chbAutorun->setCheckState(Qt::PartiallyChecked);
+    ui.chbAutorun->setCheckState(Qt::Unchecked);
 #else
   ui.chbAutorun->setEnabled(false);
 #endif
@@ -31,7 +29,7 @@ void MiscOptionsWidget::apply()
   QSettings reg("HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Run", QSettings::NativeFormat);
   if (ui.chbAutorun->checkState() == Qt::Checked)
     reg.setValue(CLIENT_NAME, QDir::toNativeSeparators(QApplication::applicationFilePath()));
-  else if (ui.chbAutorun->checkState() == Qt::Unchecked)
-    reg.remove(QApplication::applicationName());
+  else
+    reg.remove(CLIENT_NAME);
 #endif
 }
