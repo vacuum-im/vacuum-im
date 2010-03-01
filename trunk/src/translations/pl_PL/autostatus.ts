dd648b820d6e9a1fe7a2b1e048ac50b5b5fdb6f9
les.kamil <les.kamil@8ee3b2d0-dc07-11de-995a-ab19b33c1a40>
2010-03-01 13:11:26 +0000

    Polish translation of autostatus.ts updated.

--- trunk/src/translations/pl_PL/autostatus.ts
+++ trunk/src/translations/pl_PL/autostatus.ts
@@ -44,127 +44,127 @@
     </message>
     <message>
         <source>You can insert date and time into auto status text:</source>
-        <translation type="unfinished"></translation>
+        <translation>Możesz dodać datę oraz czas do tekstu autostatusu:</translation>
     </message>
     <message>
         <source>   %(&lt;format&gt;) - current date and time</source>
-        <translation type="unfinished"></translation>
+        <translation>   %(&lt;format&gt;) - bieżąca data i czas</translation>
     </message>
     <message>
         <source>   $(&lt;format&gt;) - date and time you are idle form</source>
-        <translation type="unfinished"></translation>
+        <translation>   $(&lt;format&gt;) - data i czas początku bezczynności</translation>
     </message>
     <message>
         <source>   #(&lt;format&gt;) - time you are idle for</source>
-        <translation type="unfinished"></translation>
+        <translation>   #(&lt;format&gt;) - czas bezczynności</translation>
     </message>
     <message>
         <source>Date Format:</source>
-        <translation type="unfinished"></translation>
+        <translation>Format daty:</translation>
     </message>
     <message>
         <source>   d - the day as number without a leading zero (1 to 31)</source>
-        <translation type="unfinished"></translation>
+        <translation>   d - dzień jako liczba bez zera wiodącego (1 do 31)</translation>
     </message>
     <message>
         <source>   dd - the day as number with a leading zero (01 to 31)</source>
-        <translation type="unfinished"></translation>
+        <translation>   dd - dzień jako liczba wraz z zerem wiodącym (01 do 31)</translation>
     </message>
     <message>
         <source>   ddd - the abbreviated localized day name (e.g. &apos;Mon&apos; to &apos;Sun&apos;)</source>
-        <translation type="unfinished"></translation>
+        <translation>   ddd - skrót nazwy dnia (&apos;Pon&apos; do &apos;Nie&apos;)</translation>
     </message>
     <message>
         <source>   dddd - the long localized day name (e.g. &apos;Monday&apos; to &apos;Sunday&apos;)</source>
-        <translation type="unfinished"></translation>
+        <translation>   dddd - pełna nazwa dnia (&apos;Poniedziałek&apos; - &apos;Niedziela&apos;)</translation>
     </message>
     <message>
         <source>   M - the month as number without a leading zero (1-12)</source>
-        <translation type="unfinished"></translation>
+        <translation>   M - miesiąc jako liczba bez zera wiodącego (1-12)</translation>
     </message>
     <message>
         <source>   MM - the month as number with a leading zero (01-12)</source>
-        <translation type="unfinished"></translation>
+        <translation>   MM - miesiąc jako liczba wraz z zerem wiodącym (01 do 12)</translation>
     </message>
     <message>
         <source>   MMM - the abbreviated localized month name (e.g. &apos;Jan&apos; to &apos;Dec&apos;)</source>
-        <translation type="unfinished"></translation>
+        <translation>   MMM - skrót nazwy miesiąca (&apos;Sty&apos; do &apos;Gru&apos;)</translation>
     </message>
     <message>
         <source>   MMMM - the long localized month name (e.g. &apos;January&apos; to &apos;December&apos;)</source>
-        <translation type="unfinished"></translation>
+        <translation>   MMMM - pełna nazwa miesiąca (&apos;Styczeń&apos; - &apos;Grudzień&apos;)</translation>
     </message>
     <message>
         <source>   yy - the year as two digit number (00-99)</source>
-        <translation type="unfinished"></translation>
+        <translation>   yy - rok w postaci dwucyfrowej (00 - 99)</translation>
     </message>
     <message>
         <source>   yyyy - the year as four digit number</source>
-        <translation type="unfinished"></translation>
+        <translation>   yyyy - rok w pełnej postaci</translation>
     </message>
     <message>
         <source>Time Format:</source>
-        <translation type="unfinished"></translation>
+        <translation>Format czasu:</translation>
     </message>
     <message>
         <source>   h - the hour without a leading zero (0 to 23 or 1 to 12 if AM/PM display)</source>
-        <translation type="unfinished"></translation>
+        <translation>   h - godzina bez zera wiodącego (0 do 23 lub 1 do 12 AM/PM)</translation>
     </message>
     <message>
         <source>   hh - the hour with a leading zero (00 to 23 or 01 to 12 if AM/PM display)</source>
-        <translation type="unfinished"></translation>
+        <translation>   hh - godzina wraz z zerem wiodącym (01 do 23 lub 01 do 12 AM/PM)</translation>
     </message>
     <message>
         <source>   H - the hour without a leading zero (0 to 23, even with AM/PM display)</source>
-        <translation type="unfinished"></translation>
+        <translation>   H - godzina bez zera wiodącego (0 do 23 nawet przy wyborze trybu AM/PM)</translation>
     </message>
     <message>
         <source>   HH - the hour with a leading zero (00 to 23, even with AM/PM display)</source>
-        <translation type="unfinished"></translation>
+        <translation>   HH - godzina z zerem wiodącym (00 do 23 nawet przy wyborze trybu AM/PM)</translation>
     </message>
     <message>
         <source>   m - the minute without a leading zero (0 to 59)</source>
-        <translation type="unfinished"></translation>
+        <translation>   m - minuty bez zera wiodącego (0 do 59)</translation>
     </message>
     <message>
         <source>   mm - the minute with a leading zero (00 to 59)</source>
-        <translation type="unfinished"></translation>
+        <translation>   mm - minuty z zerem wiodącym (00 do 59)</translation>
     </message>
     <message>
         <source>   s - the second without a leading zero (0 to 59)</source>
-        <translation type="unfinished"></translation>
+        <translation>   s - sekundy bez zera wiodącego (0 do 59)</translation>
     </message>
     <message>
         <source>   ss - the second with a leading zero (00 to 59)</source>
-        <translation type="unfinished"></translation>
+        <translation>   ss - sekundy z zerem wiodącym (00 do 59)</translation>
     </message>
     <message>
         <source>   z - the milliseconds without leading zeroes (0 to 999)</source>
-        <translation type="unfinished"></translation>
+        <translation>   z - milisekundy bez zer wiodących (0 do 999)</translation>
     </message>
     <message>
         <source>   zzz - the milliseconds with leading zeroes (000 to 999)</source>
-        <translation type="unfinished"></translation>
+        <translation>   zzz - milisekundy z zerami wiodącym (00 do 999)</translation>
     </message>
     <message>
         <source>   AP or A - interpret as an AM/PM time. AP must be either &apos;AM&apos; or &apos;PM&apos;</source>
-        <translation type="unfinished"></translation>
+        <translation>   AP lub A - oznacza czas w trybie AM/PM. AP musi być albo &apos;AM&apos; albo &apos;PM&apos;</translation>
     </message>
     <message>
         <source>   ap or a - interpret as an AM/PM time. ap must be either &apos;am&apos; or &apos;pm&apos;</source>
-        <translation type="unfinished"></translation>
+        <translation>   ap lub a - oznacza czas w trybie AM/PM. ap musi być albo &apos;am&apos; albo &apos;pm&apos;</translation>
     </message>
     <message>
         <source>Example:</source>
-        <translation type="unfinished"></translation>
+        <translation>Przykład:</translation>
     </message>
     <message>
         <source>Auto Status</source>
-        <translation type="unfinished">Autostatus</translation>
+        <translation>Autostatus</translation>
     </message>
     <message>
         <source>   Status is set to &apos;away&apos; at %(hh:mm:ss), because of idle from $(hh:mm:ss) for #(mm) minutes and #(ss) seconds</source>
-        <translation type="unfinished"></translation>
+        <translation>   Status ustawiony jako &apos;odszedłem&apos; o %(hh:mm:ss), z powodu bezczynności od $(hh:mm:ss) przez #(mm) minut i #(ss) sekund</translation>
     </message>
 </context>
 <context>
@@ -179,7 +179,7 @@
     </message>
     <message>
         <source>Help</source>
-        <translation type="unfinished"></translation>
+        <translation>Pomoc</translation>
     </message>
 </context>
 </TS>
