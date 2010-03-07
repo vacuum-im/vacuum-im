88e65fff636e82856c1826e04945449cf1cc03ac
potapov.s.a <potapov.s.a@8ee3b2d0-dc07-11de-995a-ab19b33c1a40>
2010-03-07 08:20:36 +0000

    *1.0.1*

--- trunk/src/definations/version.h
+++ trunk/src/definations/version.h
@@ -2,7 +2,7 @@
 #define DEF_VERSION_H
 
 #define CLIENT_NAME             "Vacuum IM"
-#define CLIENT_VERSION          "1.0.0"
+#define CLIENT_VERSION          "1.0.1"
 #define CLIENT_VERSION_SUFIX    ""
 #define CLIENT_HOME_PAGE        "http://vacuum-im.googlecode.com"
 
