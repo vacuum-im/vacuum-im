868f2c2040658778b56cabd84bc87b2a71a008f8
Maxim Ignatenko <gelraen.ua@gmail.com>
2010-08-13 09:05:30 +0000

    Локализация для PEP-плагина

---
+++ src/translations/pl_PL/pep.ts
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>PEPManager</name>
+    <message>
+        <source>PEP Manager</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Allows other plugins to recieve and publish PEP events</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
