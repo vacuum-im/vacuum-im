9b6bfca0f4c39bd5ed23c56ccfde45aa661a769f
gelraen.ua <gelraen.ua@8ee3b2d0-dc07-11de-995a-ab19b33c1a40>
2010-08-25 22:44:31 +0000

    Явное указание link_directories() не нужно, CMake указывает полные пути к библиотекам при линковке.

--- trunk/plugins.cmake
+++ trunk/plugins.cmake
@@ -16,7 +16,6 @@ if (IS_ENABLED)
 	set(QT_USE_QTXML YES)
 	include(${QT_USE_FILE})
 
-	link_directories("${CMAKE_BINARY_DIR}/src/libs")
 	include_directories("${CMAKE_SOURCE_DIR}/src"
 		"${CMAKE_BINARY_DIR}/src/plugins/${PLUGIN_NAME}"
 		".")
