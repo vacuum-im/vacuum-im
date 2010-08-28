4584d61647760a37f255a91a8ade82ff35860379
borisov.mikhail <borisov.mikhail@8ee3b2d0-dc07-11de-995a-ab19b33c1a40>
2010-08-28 20:55:57 +0000

    Исправлена сборка с cmake.

--- trunk/src/plugins/optionsmanager/optionsmanager.cmake
+++ trunk/src/plugins/optionsmanager/optionsmanager.cmake
@@ -1,3 +1,3 @@
-set(SOURCES optionsdialog.cpp optionswidget.cpp optionsmanager.cpp editprofilesdialog.cpp logindialog.cpp optionscontainer.cpp )
-set(HEADERS editprofilesdialog.h logindialog.h optionswidget.h optionscontainer.h optionsdialog.h optionsmanager.h )
+set(SOURCES optionsdialog.cpp optionswidget.cpp optionsmanager.cpp editprofilesdialog.cpp logindialog.cpp )
+set(HEADERS editprofilesdialog.h logindialog.h optionswidget.h optionsdialog.h optionsmanager.h )
 set(UIS optionsdialog.ui editprofilesdialog.ui logindialog.ui )
