fa8e2414e30661a8dd019fc9a135a8af91d392f3
Sergey Potapov <potapov.s.a@gmail.com>
2010-09-02 13:00:48 +0000

    Поправлена сборка cmake-ом

--- src/plugins/vcard/vcard.cmake
+++ src/plugins/vcard/vcard.cmake
@@ -1,3 +1,3 @@
-set(SOURCES vcardplugin.cpp vcarddialog.cpp vcard.cpp edititemdialog.cpp )
-set(HEADERS vcarddialog.h vcard.h vcardplugin.h edititemdialog.h )
+set(SOURCES vcardplugin.cpp vcarddialog.cpp vcard.cpp edititemdialog.cpp prixmapframe.cpp)
+set(HEADERS vcarddialog.h vcard.h vcardplugin.h edititemdialog.h prixmapframe.h)
 set(UIS edititemdialog.ui vcarddialog.ui )
