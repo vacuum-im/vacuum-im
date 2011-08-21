7520752f544b1d2bc263a2796fea7d6c44d8e37c
Sergey Potapov <potapov.s.a@gmail.com>
2011-08-21 16:21:59 +0000

    Версия изменена на 1.2.0 Alpha, чтобы версия в trunk не была меньше версии в стабильной ветке.

--- src/definitions/version.h
+++ src/definitions/version.h
@@ -2,8 +2,8 @@
 #define DEF_VERSION_H
 
 #define CLIENT_NAME             "Vacuum-IM"
-#define CLIENT_VERSION          "1.1.0"
-#define CLIENT_VERSION_SUFIX    ""
+#define CLIENT_VERSION          "1.2.0"
+#define CLIENT_VERSION_SUFIX    "Alpha"
 #define CLIENT_HOME_PAGE        "http://vacuum-im.googlecode.com"
 
 #endif
