fff08eb3929dddf7c3f3f4f150e6ae7258a5fca4
Sergey Potapov <potapov.s.a@gmail.com>
2011-08-22 11:02:51 +0000

    Реализовано автоматическое выделение текста под курсором при вызове контекстного в окне просмотра сообщений для упрощенного стиля сообщений.

--- src/plugins/simplemessagestyle/styleviewer.cpp
+++ src/plugins/simplemessagestyle/styleviewer.cpp
@@ -1,5 +1,9 @@
 #include "styleviewer.h"
 
+#include <QTextBlock>
+#include <QMouseEvent>
+#include <QTextDocumentFragment>
+
 StyleViewer::StyleViewer(QWidget *AParent) : QTextBrowser(AParent)
 {
 	setOpenLinks(false);
@@ -11,4 +15,34 @@ StyleViewer::StyleViewer(QWidget *AParent) : QTextBrowser(AParent)
 StyleViewer::~StyleViewer()
 {
 
+}
+
+void StyleViewer::mouseReleaseEvent(QMouseEvent *AEvent)
+{
+   if (AEvent->button() == Qt::RightButton)
+   {
+      QTextCursor cursor = cursorForPosition(AEvent->pos());
+      if (textCursor().selection().isEmpty() || textCursor().selectionStart()>cursor.position() || textCursor().selectionEnd()<cursor.position())
+      {
+         if (!anchorAt(AEvent->pos()).isEmpty())
+         {
+            QTextBlock block = cursor.block();
+            for (QTextBlock::iterator it = block.begin(); !it.atEnd(); it++)
+            {
+               if (it.fragment().contains(cursor.position()))
+               {
+                  cursor.setPosition(it.fragment().position());
+                  cursor.movePosition(QTextCursor::NextCharacter,QTextCursor::KeepAnchor,it.fragment().length());
+                  break;
+               }
+            }
+         }
+         else
+         {
+            cursor.select(QTextCursor::WordUnderCursor);
+         }
+         setTextCursor(cursor);
+      }
+   }
+   QTextBrowser::mouseReleaseEvent(AEvent);
 }
