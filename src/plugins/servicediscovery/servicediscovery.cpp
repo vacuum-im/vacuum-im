3830057d6057c3f2074fbbadef7468ea51563245
Sergey Potapov <potapov.s.a@gmail.com>
2011-10-16 16:46:37 +0000

    В плагине ServiceDiscovery использовался интерфейс IRosterPlugin без проверки его наличия.

--- src/plugins/servicediscovery/servicediscovery.cpp
+++ src/plugins/servicediscovery/servicediscovery.cpp
@@ -1205,7 +1205,7 @@ void ServiceDiscovery::onStreamOpened(IXmppStream *AXmppStream)
 	requestDiscoInfo(AXmppStream->streamJid(),streamDomane);
 	requestDiscoItems(AXmppStream->streamJid(),streamDomane);
 
-	IRoster *roster = FRosterPlugin->findRoster(AXmppStream->streamJid());
+	IRoster *roster = FRosterPlugin!=NULL ? FRosterPlugin->findRoster(AXmppStream->streamJid()) : NULL;
 	QList<IRosterItem> ritems = roster!=NULL ? roster->rosterItems() : QList<IRosterItem>();
 	foreach(IRosterItem ritem, ritems)
 	{
