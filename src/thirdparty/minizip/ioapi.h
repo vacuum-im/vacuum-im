adc76739b48830b8c94c2008f4c1e29e61f6b25e
Maxim Ignatenko <gelraen.ua@gmail.com>
2012-01-24 08:59:52 +0000

    Удалены \r и исправлена сборка на FreeBSD

--- src/thirdparty/minizip/ioapi.h
+++ src/thirdparty/minizip/ioapi.h
@@ -53,6 +53,10 @@
 #define fopen64 fopen
 #define ftello64 ftell
 #define fseeko64 fseek
+#elif defined(__FreeBSD__)
+#define fopen64 fopen
+#define ftello64 ftello
+#define fseeko64 fseeko
 #else
 #ifdef _MSC_VER
  #define fopen64 fopen
