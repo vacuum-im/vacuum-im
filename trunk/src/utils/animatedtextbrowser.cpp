fef952657a65f3fa2b101bc800a399712aa2c175
potapov.s.a <potapov.s.a@8ee3b2d0-dc07-11de-995a-ab19b33c1a40>
2012-02-12 09:27:40 +0000

    Оптимизирована анимация в QTextEdit, но все-равно еще сильно тормозит.

--- trunk/src/utils/animatedtextbrowser.cpp
+++ trunk/src/utils/animatedtextbrowser.cpp
@@ -1,14 +1,19 @@
 #include "animatedtextbrowser.h"
 
 #include <QBuffer>
+#include <QTextBlock>
 #include <QTextLayout>
 #include <QNetworkReply>
 
 AnimatedTextBrowser::AnimatedTextBrowser(QWidget *AParent) : QTextBrowser(AParent)
 {
-	FBusy = false;
 	FAnimated = false;
 	FNetworkAccessManager = NULL;
+
+	FUpdateTimer.setSingleShot(true);
+	connect(&FUpdateTimer,SIGNAL(timeout()),SLOT(onUpdateDocumentAnimation()));
+
+	connect(document(),SIGNAL(contentsChange(int, int, int)),SLOT(onDocumentContentsChanged(int,int,int)));
 }
 
 bool AnimatedTextBrowser::isAnimated()
@@ -26,40 +31,10 @@ void AnimatedTextBrowser::setAnimated(bool AAnimated)
 			if (!FAnimated)
 				movie->jumpToFrame(0);
 			movie->setPaused(!FAnimated);
+			FChangedMovies += movie;
 		}
+		FUpdateTimer.start(0);
 	}
-}
-
-QPixmap AnimatedTextBrowser::addAnimation(const QUrl& AUrl, const QVariant &AImageData)
-{
-	QMovie *movie = NULL;
-	switch (AImageData.type())
-	{
-	case QVariant::String:
-		{
-			movie = new QMovie(AImageData.toString(), QByteArray(), this);
-			break;
-		}
-	case QVariant::ByteArray:
-		{
-			QBuffer *buffer = new QBuffer();
-			buffer->setData(AImageData.toByteArray());
-			buffer->open(QBuffer::ReadOnly);
-			movie = new QMovie(buffer, QByteArray(), this);
-			buffer->setParent(movie);
-			break;
-		}
-	default:
-		return QPixmap(); // Nothing to do
-	}
-	
-	FUrls.insert(movie, AUrl);
-	connect(movie, SIGNAL(frameChanged(int)), SLOT(onAnimationFrameChanged()));
-	
-	movie->start();
-	movie->setPaused(!FAnimated);
-
-	return movie->currentPixmap();
 }
 
 QNetworkAccessManager *AnimatedTextBrowser::networkAccessManager() const
@@ -72,47 +47,114 @@ void AnimatedTextBrowser::setNetworkAccessManager(QNetworkAccessManager *ANetwor
 	FNetworkAccessManager=ANetworkAccessManager;
 }
 
-QVariant AnimatedTextBrowser::loadResource(int AType, const QUrl &AName)
+QList<int> AnimatedTextBrowser::findUrlPositions(const QUrl &AName) const
 {
-	FBusy = true;   // Set busy flag to prevent crash
+	QList<int> positions;
 
-	QVariant result;
-	QString nameString = AName.toString();
-
-	if (!FElements.contains(nameString))                     // Resource is not loaded yet
+	QTextBlock block = document()->firstBlock();
+	while (block.isValid())
 	{
-		result = QTextBrowser::loadResource(AType, AName);    // Try default resource loader
-		if (FNetworkAccessManager && result.isNull())         // If got nothing Try using Network Access Manager if available
+		for (QTextBlock::iterator it = block.begin(); !it.atEnd(); it++)
 		{
-			FElements.insert(nameString, QVariant());    // Mark resource as "loading" by inserting an empty QVariant into hash table
-			QNetworkReply *reply = FNetworkAccessManager->get(QNetworkRequest(AName));   // Start loading resource
-			connect(reply, SIGNAL(finished()), SLOT(onResourceLoadFinished()));
+			QTextFragment fragment = it.fragment();
+			if (fragment.charFormat().isImageFormat())
+			{
+				if (AName == fragment.charFormat().toImageFormat().name())
+					positions.append(fragment.position());
+			}
+		}
+		block = block.next();
+	}
+
+	return positions;
+}
+
+QPixmap AnimatedTextBrowser::addAnimation(const QUrl &AName, const QVariant &AImageData)
+{
+	QMovie *movie = FUrls.key(AName);
+	if (movie == NULL)
+	{
+		switch (AImageData.type())
+		{
+		case QVariant::String:
+			{
+				movie = new QMovie(AImageData.toString(), QByteArray(), this);
+				break;
+			}
+		case QVariant::ByteArray:
+			{
+				QBuffer *buffer = new QBuffer();
+				buffer->setData(AImageData.toByteArray());
+				buffer->open(QBuffer::ReadOnly);
+				movie = new QMovie(buffer, QByteArray(), this);
+				buffer->setParent(movie);
+				break;
+			}
+		default:
+			break;
+		}
+
+		if (movie != NULL)
+		{
+			FUrls.insert(movie, AName);
+			FUrlPositions.insert(movie,findUrlPositions(AName));
+			connect(movie,SIGNAL(frameChanged(int)),SLOT(onAnimationFrameChanged()));
+			connect(movie,SIGNAL(destroyed(QObject *)),SLOT(onMovieDestroyed(QObject *)));
+
+			movie->start();
+			movie->setPaused(!FAnimated);
 		}
 	}
 	else
 	{
-		result = FElements.value(nameString);
+		FUrlPositions[movie] = findUrlPositions(AName);
 	}
+	return movie!=NULL ? movie->currentPixmap() : QPixmap();
+}
 
-	if (!result.isNull())
-		result=addAnimation(AName, result.toByteArray());
+void AnimatedTextBrowser::showEvent(QShowEvent *AEvent)
+{
+	FUpdateTimer.start(0);
+	QTextBrowser::showEvent(AEvent);
+}
 
-	FBusy = false;
-	return result;
+QVariant AnimatedTextBrowser::loadResource(int AType, const QUrl &AName)
+{
+	if (AType == QTextDocument::ImageResource)
+	{
+		QVariant result;
+		QString nameString = AName.toString();
+
+		if (!FResources.contains(nameString))                   // Resource is not loaded yet
+		{
+			result = QTextBrowser::loadResource(AType, AName);    // Try default resource loader
+			if (FNetworkAccessManager && result.isNull())         // If got nothing Try using Network Access Manager if available
+			{
+				FResources.insert(nameString, QVariant());          // Mark resource as "loading" by inserting an empty QVariant into hash table
+				QNetworkReply *reply = FNetworkAccessManager->get(QNetworkRequest(AName));   // Start loading resource
+				connect(reply, SIGNAL(finished()), SLOT(onResourceLoadFinished()));
+			}
+		}
+		else
+		{
+			result = FResources.value(nameString);
+		}
+
+		if (!result.isNull())
+			result = addAnimation(AName, result.toByteArray());
+
+		return result;
+	}
+	return QTextBrowser::loadResource(AType, AName);
 }
 
 void AnimatedTextBrowser::onAnimationFrameChanged()
 {
-	if (!FBusy)   // Skip frame if currently busy
+	QMovie *movie = qobject_cast<QMovie *>(sender());
+	if (movie)
 	{
-		QMovie* movie = qobject_cast<QMovie *>(sender());
-		if (movie)
-		{
-			QUrl url = FUrls.value(movie);
-			document()->addResource(QTextDocument::ImageResource, url, movie->currentPixmap());
-			emit resourceAdded(url);
-			setLineWrapColumnOrWidth(lineWrapColumnOrWidth()); // causes reload
-		}
+		FChangedMovies += movie;
+		FUpdateTimer.start(0);
 	}
 }
 
@@ -122,9 +164,82 @@ void AnimatedTextBrowser::onResourceLoadFinished()
 	if (reply->error() == QNetworkReply::NoError)
 	{
 		QByteArray data = reply->readAll();
-		FElements.insert(reply->url().toString(), data);
+		FResources.insert(reply->url().toString(), data);
 		emit resourceLoaded(reply->url());
-		setLineWrapColumnOrWidth(lineWrapColumnOrWidth()); // causes reload
+		setLineWrapColumnOrWidth(lineWrapColumnOrWidth());
 	}
 	reply->deleteLater();
+}
+
+void AnimatedTextBrowser::onUpdateDocumentAnimation()
+{
+	if (isVisible())
+	{
+		static const int minUpdateTimeout = qRound(1000/15.0);
+		qint64 timeout = FLastUpdate.isValid() ? FLastUpdate.msecsTo(QDateTime::currentDateTime()) : minUpdateTimeout;
+		if (timeout >= minUpdateTimeout)
+		{
+			QList<int> dirtyBlocks;
+			document()->blockSignals(true);
+			QTextCursor cursor(document());
+			cursor.beginEditBlock();
+			foreach(QMovie *movie, FChangedMovies)
+			{
+				QUrl url = FUrls.value(movie);
+				document()->addResource(QTextDocument::ImageResource, url, movie->currentPixmap());
+
+				foreach(int pos, FUrlPositions.value(movie))
+				{
+					cursor.setPosition(pos);
+					int block = document()->findBlock(pos).blockNumber();
+					if (!dirtyBlocks.contains(block))
+					{
+						cursor.movePosition(QTextCursor::NextCharacter,QTextCursor::KeepAnchor);
+						cursor.insertImage(cursor.charFormat().toImageFormat());
+						dirtyBlocks.append(block);
+					}
+				}
+				emit resourceUpdated(url);
+			}
+			cursor.endEditBlock();
+			document()->blockSignals(false);
+
+			FChangedMovies.clear();
+			FLastUpdate = QDateTime::currentDateTime();
+		}
+		else
+		{
+			FUpdateTimer.start(minUpdateTimeout-timeout+1);
+		}
+	}
+}
+
+void AnimatedTextBrowser::onMovieDestroyed(QObject *AObject)
+{
+	QMovie *movie = static_cast<QMovie *>(AObject);
+	if (movie)
+	{
+		FChangedMovies -= movie;
+		FUrls.remove(movie);
+		FUrlPositions.remove(movie);
+	}
+}
+
+void AnimatedTextBrowser::onDocumentContentsChanged(int APosition, int ARemoved, int AAdded)
+{
+	for(QHash<QMovie *, QList<int> >::iterator it=FUrlPositions.begin(); it!=FUrlPositions.end(); it++)
+	{
+		QList<int> &positions = it.value();
+		for(int i=0; i<positions.count(); i++)
+		{
+			int pos = positions.at(i);
+			if (pos >= APosition)
+			{
+				if (pos < APosition+ARemoved)
+					positions.removeAt(i--);            // Position was removed // TODO: Destroy movie if positions is empty
+				else
+					positions[i] += AAdded-ARemoved;    // Updating image position
+			}
+		}
+	}
 }
