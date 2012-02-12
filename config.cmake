02c52b5ba7180fa55fc579c673c851510f162d88
Sergey Potapov <potapov.s.a@gmail.com>
2012-02-12 09:27:40 +0000

    Оптимизирована анимация в QTextEdit, но все-равно еще сильно тормозит.

--- config.cmake
+++ config.cmake
@@ -2,7 +2,7 @@ set(TARGET_LOADER vacuum)
 set(TARGET_UTILS vacuumutils)
 
 if (UNIX)
-	set(VERSION_UTILS 1.11.0)
+	set(VERSION_UTILS 1.11.1)
 	set(VERSION_UTILS_ABI 1.11)
 endif (UNIX)
 
--- src/utils/animatedtextbrowser.h
+++ src/utils/animatedtextbrowser.h
@@ -1,7 +1,10 @@
 #ifndef ANIMATEDTEXTBROWSER_H
 #define ANIMATEDTEXTBROWSER_H
 
+#include <QSet>
 #include <QMovie>
+#include <QTimer>
+#include <QDateTime>
 #include <QTextBrowser>
 #include <QNetworkAccessManager>
 #include "utilsexport.h"
@@ -14,22 +17,31 @@ public:
 	AnimatedTextBrowser(QWidget *AParent = NULL);
 	bool isAnimated();
 	void setAnimated(bool AAnimated);    
-	QPixmap addAnimation(const QUrl& AUrl, const QVariant &AImageData);
 	QNetworkAccessManager *networkAccessManager() const;
 	void setNetworkAccessManager(QNetworkAccessManager *ANetworkAccessManager);
 signals:
-	void resourceAdded(QUrl AName);
-	void resourceLoaded(QUrl AName);
+	void resourceLoaded(const QUrl &AName);
+	void resourceUpdated(const QUrl &AName);
 protected:
+	QList<int> findUrlPositions(const QUrl &AName) const;
+	QPixmap addAnimation(const QUrl &AName, const QVariant &AImageData);
+protected:
+	virtual void showEvent(QShowEvent *AEvent);
 	virtual QVariant loadResource(int AType, const QUrl &AName);
 protected slots:
 	void onAnimationFrameChanged();
-	void onResourceLoadFinished();    
+	void onResourceLoadFinished();
+	void onUpdateDocumentAnimation();
+	void onMovieDestroyed(QObject *AObject);
+	void onDocumentContentsChanged(int APosition, int ARemoved, int AAdded);
 private:
-	bool FBusy;
 	bool FAnimated;
-	QHash<QMovie*, QUrl> FUrls;
-	QHash<QString, QVariant> FElements;
+	QTimer FUpdateTimer;
+	QDateTime FLastUpdate;
+	QSet<QMovie *> FChangedMovies;
+	QHash<QMovie *, QUrl> FUrls;
+	QHash<QMovie *, QList<int> > FUrlPositions;
+	QHash<QString, QVariant> FResources;
 	QNetworkAccessManager *FNetworkAccessManager;
 };
 
