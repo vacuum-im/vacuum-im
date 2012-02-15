6587bb82bb0d037f74363761fc03722fd2e9aa23
Sergey Potapov <potapov.s.a@gmail.com>
2012-02-15 20:32:27 +0000

    Добавлены скрипты сборки пакетов для Slackware от StreamThreader.

---
+++ src/packages/linux/slackware/slackbuild.sh
@@ -0,0 +1,143 @@
+#!/bin/bash
+
+#
+# Author Oleg A Deordiev
+# Version 1.01
+#
+
+arche=$HOSTTYPE
+CWD="$(pwd)"
+startdir="$CWD"
+srcdir="$startdir"
+builddir="$srcdir"/build
+pkgdir="$srcdir"/pkgdir
+instldir="$srcdir"/instldir
+cpucore=""
+threadsmy=""
+pkgtype=txz
+revpkg="1"
+CLIENT_NAME=""
+CLIENT_VERSION=""
+CLIENT_VERSION_SUFIX=""
+CLIENT_SVN_VER=""
+
+# Go to root
+cd ../../../../
+
+# If no correct position
+if [ ! -d ./src ]
+then
+	echo "Not found dir src, maybe not correct folder"
+	exit 1
+fi
+
+CWD="$(pwd)"
+srcdir="$CWD"
+builddir="$srcdir"/build
+pkgdir="$srcdir"/pkgdir
+instldir="$srcdir"/instldir
+
+CLIENT_NAME="$(grep "CLIENT_NAME" ./src/definitions/version.h | awk -F '"' '{print $2}' | awk -F '"' '{print $1}' | tr -d '-')"
+CLIENT_VERSION="$(grep -m 1 "CLIENT_VERSION" ./src/definitions/version.h | awk -F '"' '{print $2}' | awk -F '"' '{print $1}')"
+CLIENT_VERSION_SUFIX="$(grep "CLIENT_VERSION_SUFIX" ./src/definitions/version.h | awk -F '"' '{print $2}' | awk -F '"' '{print $1}')"
+
+if [ -d "$srcdir"/.svn ]
+then
+	echo "This is a svn version"
+	# Get svn version
+	CLIENT_SVN_VER="$(sed -n -e '/^dir$/{n;p;q;}' .svn/entries)"
+fi
+
+# Check dir for cmake
+if [ ! -d "$instldir" ]
+then
+	mkdir -p "$instldir"
+fi
+
+# Go to cmake dir
+cd "$instldir"
+
+# Run cmake
+if [ "$arche" == "x86_64" ]
+then
+	echo "Compile for "$arche" architecture"
+	if ! cmake -DCMAKE_INSTALL_PREFIX=/usr -DINSTALL_LIB_DIR=lib64 .. > /dev/null 2>&1
+		then
+		echo "Eror - cmake"
+		exit 1
+	fi
+else
+	echo "Compile for "$arche" architecture"
+	if ! cmake -DCMAKE_INSTALL_PREFIX=/usr -DINSTALL_LIB_DIR=lib .. > /dev/null 2>&1
+	then
+		echo "Error - cmake"
+		exit 1
+	fi
+fi
+
+# Get cpu number
+cpucore="$(cat /proc/cpuinfo | grep -m 1 "cpu cores" | awk -F": " '{print $2}')"
+
+# Check CPU function
+if [ "$cpucore" == "" ]
+then
+	echo "Failed to get CPU number"
+	echo "Set CPU number to 1"
+	cpucore=1
+fi
+
+echo "Make program in "$cpucore" CPU"
+
+# Make
+if ! make -j "$cpucore" > /dev/null 2>&1
+then
+	echo "Erorr - make"
+	exit 1
+fi
+
+if [ ! -f $builddir ]
+then
+	mkdir -p $builddir
+fi
+
+# Install to fake root dir
+if ! make install DESTDIR="$builddir" > /dev/null 2>&1
+then
+	echo "Error - make install"
+	exit 1
+fi
+
+# Go to complited version
+cd $builddir
+
+# Create dir for slackware package manager
+mkdir install
+
+# Include special files for slackpkg
+cp "$startdir"/slack-desc ./install/slack-desc
+cp "$startdir"/slack-required ./install/slack-required
+
+# Create dir for complited package
+if [ ! -d "$pkgdir" ]
+then
+	mkdir -p "$pkgdir"
+fi
+
+# Create package for Slackware Linux
+if makepkg -l y -p -c y "$pkgdir"/"$CLIENT_NAME"-"$CLIENT_VERSION""$CLIENT_VERSION_SUFIX""$CLIENT_SVN_VER"-"$arche"-"$revpkg"."$pkgtype" > /dev/null 2>&1
+then
+	echo "Package created - "$pkgdir"/"$CLIENT_NAME"-"$CLIENT_VERSION""$CLIENT_VERSION_SUFIX""$CLIENT_SVN_VER"-"$arche"-"$revpkg"."$pkgtype""
+else
+	echo "Package create FAIL"
+	exit 1
+fi
+
+cd "$srcdir"
+
+# CleanUp
+rm -rf "$builddir"
+rm -rf "$instldir"
+echo "Clean"
+
+exit 0
+
