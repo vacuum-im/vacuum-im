02856af478ecc21fc2a453a32502df7748f1fd4c
potapov.s.a <potapov.s.a@8ee3b2d0-dc07-11de-995a-ab19b33c1a40>
2012-02-28 13:41:45 +0000

    Патч для сборки dmg под MacOS от Krab-a.

--- trunk/src/loader/loader.pro
+++ trunk/src/loader/loader.pro
@@ -15,6 +15,9 @@ include(loader.pri)
 win32:RC_FILE      = loader.rc
 macx:ICON          = ../../vacuum.icns
 
+#MacOS Info.plist
+macx:QMAKE_INFO_PLIST = ../../src/packages/macosx/Info.plist
+
 #SVN Info
 isEmpty(SVN_REVISION) {
   SVN_REVISION=$$system(svnversion -n -c ./../../)
@@ -58,12 +61,14 @@ unix:!macx {
 
 #MaxOS Install
 macx {
-  UTILS_LIB_NAME   = lib$${TARGET_UTILS}.1.0.0.dylib
+  UTILS_LIB_NAME  = lib$${TARGET_UTILS}.$${VERSION_UTILS}.dylib
   UTILS_LIB_LINK   = lib$${TARGET_UTILS}.1.dylib
+  UTILS_LIB_LINK_EXTERNAL_PLUGINS = lib$${TARGET_UTILS}.dylib
 
   lib_utils.path   = $$INSTALL_LIBS
   lib_utils.extra  = cp -f ../libs/$$UTILS_LIB_NAME $(INSTALL_ROOT)$$INSTALL_LIBS/$$UTILS_LIB_NAME && \
-                     ln -sf $$UTILS_LIB_NAME $(INSTALL_ROOT)$$INSTALL_LIBS/$$UTILS_LIB_LINK
+                     ln -sf $$UTILS_LIB_NAME $(INSTALL_ROOT)$$INSTALL_LIBS/$$UTILS_LIB_LINK && \
+                     ln -sf $$UTILS_LIB_NAME $(INSTALL_ROOT)$$INSTALL_LIBS/$$UTILS_LIB_LINK_EXTERNAL_PLUGINS
   INSTALLS        += lib_utils
 
   name_tool.path   = $$INSTALL_BINS
--- trunk/src/packages/macosx/make_dmg.sh
+++ trunk/src/packages/macosx/make_dmg.sh
@@ -40,7 +40,7 @@ ORIG_NAME="vacuum"
 SVN_ROOT="../../.." 
 
 # Name of bundle, as it'll be included in .dmg
-TARGET_NAME="Vacuum-IM.app" 
+PRODUCT_NAME="Vacuum-IM"
 
 # Filename of the resulting .dmg image
 DMG_NAME="${ORIG_NAME}_${VERSION}_macosx" 
@@ -57,6 +57,7 @@ DYLD_PREFIX="@executable_path/../Frameworks"
 CONTENTS_DIR="$TMP_DIR/$ORIG_NAME.app/Contents"
 FW_DIR="$CONTENTS_DIR/Frameworks"
 SYS_PLUGINS_DIR=`$PATH_TO_QMAKE -query QT_INSTALL_PLUGINS`
+SYS_TRANSLATIONS_DIR=`$PATH_TO_QMAKE -query QT_INSTALL_TRANSLATIONS`
 SYS_FW_DIR=`$PATH_TO_QMAKE -query QT_INSTALL_LIBS`
 SCRIPT_DIR=`pwd`/`dirname $0`
 
@@ -69,12 +70,14 @@ function process_binary {
 	echo -e "$tab Processing" `basename $binary`
 	
 	if [ -f "$binary" ]; then
-		for str1 in `otool -LX "$binary" | grep $SYS_FW_DIR | cut -d " " -f 1`; do
+		#for str1 in `otool -LX "$binary" | grep $SYS_FW_DIR | cut -d " " -f 1`; do
+		for str1 in `otool -LX "$binary" | grep Qt | cut -d " " -f 1`; do
 			local str=`basename "$str1"`
 			if [ `basename $binary` != $str ]; then
 				local l="$str.framework/Versions/4/$str"
 				echo -e "$tab\t Resolving dependency: $str"
-				install_name_tool -change "$SYS_FW_DIR/$l" "$DYLD_PREFIX/$l" "$binary"
+				#install_name_tool -change "$SYS_FW_DIR/$l" "$DYLD_PREFIX/$l" "$binary"
+				install_name_tool -change "$l" "$DYLD_PREFIX/$l" "$binary"
 				process_lib "$l" "$tab\t"
 			fi
 		done;
@@ -113,7 +116,7 @@ mkdir $TMP_DIR
 
 echo "Building..."
 cd $SVN_ROOT
-$PATH_TO_QMAKE -recursive -spec macx-g++ $BUILDOPTS $ORIG_NAME.pro && make && make INSTALL_ROOT=$SCRIPT_DIR install
+$PATH_TO_QMAKE -recursive -spec macx-g++ $BUILDOPTS $ORIG_NAME.pro && make -j4 && make INSTALL_ROOT=$SCRIPT_DIR install
 echo "Build done!"
 
 
@@ -130,6 +133,12 @@ echo -n "Copying Qt plugins to bundle... "
 cp -Rf "$SYS_PLUGINS_DIR/imageformats" "$CONTENTS_DIR/PlugIns"
 echo "done!"
 
+echo -n "Copying Qt locales to bundle... "
+AVAIBLE_LANGUAGES=`ls $CONTENTS_DIR/Resources/translations/`
+for lang in $AVAIBLE_LANGUAGES ; do
+	cp "$SYS_TRANSLATIONS_DIR/qt_$lang.qm" "$CONTENTS_DIR/Resources/translations/$lang/qt_$lang.qm"
+done
+echo "done!"
 
 strip "$CONTENTS_DIR/MacOS/vacuum"
 process_binary "$CONTENTS_DIR/MacOS/vacuum" ""
@@ -147,12 +156,19 @@ echo "done!"
 
 echo -n "Cleaning up bundle... "
 find "$CONTENTS_DIR/Frameworks" | egrep "debug|Headers" | xargs rm -rf
+find "$CONTENTS_DIR/Frameworks" | egrep ".prl" | xargs rm
+find "$CONTENTS_DIR" -type d -name .svn | xargs rm -rf
 echo "done!"
 
+echo -n "Set program version to Info.plist and copy InfoPlist.strings "
+sed -i.bak "s/1.0.0.0/$VERSION/" $CONTENTS_DIR/Info.plist ; rm $CONTENTS_DIR/Info.plist.bak
+mkdir $CONTENTS_DIR/Resources/en.lproj
+cp $SVN_ROOT/src/packages/macosx/InfoPlist.strings $CONTENTS_DIR/Resources/en.lproj/InfoPlist.strings
+echo "done!"
 
 echo "Creating disk image..."
-mv "$TMP_DIR/$ORIG_NAME.app" "$TMP_DIR/$TARGET_NAME"
-hdiutil create -ov -srcfolder $TMP_DIR -format UDBZ -volname "$TARGET_NAME" "$DMG_NAME.dmg"
+
+hdiutil create -ov -srcfolder $TMP_DIR -format UDBZ -volname "$PRODUCT_NAME" "$DMG_NAME.dmg"
 
 
 echo -n "Cleaning up temp folder... "
