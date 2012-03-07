e34d38a4a58f75da8a0db565210c9e54e72e9e3c
potapov.s.a <potapov.s.a@8ee3b2d0-dc07-11de-995a-ab19b33c1a40>
2012-03-07 15:26:43 +0000

    Добавлены файлы локализаций плагина FileMessageArchive.

---
+++ branches/dev_history/src/translations/de/filemessagearchive.ts
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de" sourcelanguage="en">
+<context>
+    <name>FileMessageArchive</name>
+    <message>
+        <source>File Message Archive</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Allows to save the history of communications in local files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Local File Archive</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>History of communications is stored in local files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Internal error</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WorkingThread</name>
+    <message>
+        <source>Failed to save archive collection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to remove collection file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to load collection from file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Internal error</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
