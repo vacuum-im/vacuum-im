8a90cb4a97bd92bd7aca59c1bad4be07c729ebf9
potapov.s.a <potapov.s.a@8ee3b2d0-dc07-11de-995a-ab19b33c1a40>
2012-06-08 06:35:26 +0000

    Добавлена возможность локализации домена google для более релевантного поиска.

--- trunk/src/translations/ru/messagewidgets.ts
+++ trunk/src/translations/ru/messagewidgets.ts
@@ -210,6 +210,11 @@
         <comment>Close other tabs</comment>
         <translation>Ctrl+Shift+W</translation>
     </message>
+    <message>
+        <source>google.com</source>
+        <comment>Your google domain</comment>
+        <translation>google.ru</translation>
+    </message>
 </context>
 <context>
     <name>MessageWindow</name>
--- trunk/src/translations/uk/messagewidgets.ts
+++ trunk/src/translations/uk/messagewidgets.ts
@@ -210,6 +210,11 @@
         <comment>Close other tabs</comment>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>google.com</source>
+        <comment>Your google domain</comment>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>MessageWindow</name>
