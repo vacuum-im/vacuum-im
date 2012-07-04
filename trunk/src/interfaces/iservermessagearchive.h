d643a4f35d1485a081c233cee962dd66e77d5606
potapov.s.a <potapov.s.a@8ee3b2d0-dc07-11de-995a-ab19b33c1a40>
2012-07-04 20:03:37 +0000

    Сохранены забытые изменения в IServerMesssageArchive.

--- trunk/src/interfaces/iservermessagearchive.h
+++ trunk/src/interfaces/iservermessagearchive.h
@@ -10,8 +10,8 @@ class IServerMesssageArchive :
 {
 public:
 	virtual QObject *instance() =0;
-	virtual QString loadServerHeaders(const Jid AStreamJid, const IArchiveRequest &ARequest, const IArchiveResultSet &AResult = IArchiveResultSet()) =0;
-	virtual QString loadServerCollection(const Jid AStreamJid, const IArchiveHeader &AHeader, const IArchiveResultSet &AResult = IArchiveResultSet()) =0;
+	virtual QString loadServerHeaders(const Jid &AStreamJid, const IArchiveRequest &ARequest, const IArchiveResultSet &AResult = IArchiveResultSet()) =0;
+	virtual QString loadServerCollection(const Jid &AStreamJid, const IArchiveHeader &AHeader, const IArchiveResultSet &AResult = IArchiveResultSet()) =0;
 	virtual QString loadServerModifications(const Jid &AStreamJid, const QDateTime &AStart, int ACount, const IArchiveResultSet &AResult = IArchiveResultSet()) =0;
 protected:
 	virtual void serverHeadersLoaded(const QString &AId, const QList<IArchiveHeader> &AHeaders, const IArchiveResultSet &AResult) =0;
@@ -19,6 +19,6 @@ protected:
 	virtual void serverModificationsLoaded(const QString &AId, const IArchiveModifications &AModifs, const IArchiveResultSet &AResult) =0;
 };
 
-Q_DECLARE_INTERFACE(IServerMesssageArchive,"Vacuum.Plugin.IServerMesssageArchive/1.0")
+Q_DECLARE_INTERFACE(IServerMesssageArchive,"Vacuum.Plugin.IServerMesssageArchive/1.1")
 
 #endif //ISERVERMESSAGEARCHIVE_H
