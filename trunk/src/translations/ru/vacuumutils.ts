f24e4ce8a21eea162b690dc4bd065f9cec8eeea9
potapov.s.a <potapov.s.a@8ee3b2d0-dc07-11de-995a-ab19b33c1a40>
2012-08-27 16:20:12 +0000

    Добавлены костыли для корректного отображения кнопки очистки поля поиска в теме "Oxygen", иконка для этой кнопки берется по возможности из системной темы.

--- trunk/src/translations/ru/vacuumutils.ts
+++ trunk/src/translations/ru/vacuumutils.ts
@@ -2,6 +2,17 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="ru" sourcelanguage="en">
 <context>
+    <name>SearchLineEdit</name>
+    <message>
+        <source>Search options</source>
+        <translation>Настройки поиска</translation>
+    </message>
+    <message>
+        <source>Clear text</source>
+        <translation>Очистить текст</translation>
+    </message>
+</context>
+<context>
     <name>XmppStreamError</name>
     <message>
         <source>Undefined error condition</source>
