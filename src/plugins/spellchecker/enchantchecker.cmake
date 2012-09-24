906f7cf6b635720c22a7814381681309cde0b807
Alexey Ivanov <alexey.ivanes@gmail.com>
2012-09-24 12:40:51 +0000

    CMake: Одна переменная SPELLCHECKER_BACKEND={aspell,enchant,macspell} для выбора движка проверки, вместо USE_{ASPELL,ENCHANT,MACSPELL}

--- src/plugins/spellchecker/enchantchecker.cmake
+++ src/plugins/spellchecker/enchantchecker.cmake
@@ -2,6 +2,8 @@ add_definitions(-DHAVE_ENCHANT)
 
 set(ADD_LIBS enchant)
 
+message(STATUS "Spellchecker backend: enchant")
+
 set(SOURCES ${SOURCES} "enchantchecker.cpp")
 set(HEADERS ${HEADERS} "enchantchecker.h")
 
--- src/plugins/spellchecker/hunspellchecker.cmake
+++ src/plugins/spellchecker/hunspellchecker.cmake
@@ -3,8 +3,11 @@ add_definitions(-DHUNSPELL_STATIC)
 
 if (SYSTEM_HUNSPELL_FOUND)
 	set(ADD_LIBS ${SYSTEM_HUNSPELL_FOUND})
+	add_definitions(-DUSE_SYSTEM_HUNSPELL)
+	message(STATUS "Spellchecker backend: system hunspell, found at ${SYSTEM_HUNSPELL_FOUND}")
 else (SYSTEM_HUNSPELL_FOUND)
 	set(ADD_LIBS hunspell)
+	message(STATUS "Spellchecker backend: bundled hunspell")
 endif (SYSTEM_HUNSPELL_FOUND)
 
 set(SOURCES ${SOURCES} "hunspellchecker.cpp")
