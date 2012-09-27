e53590c78f2404cefc7d3baa2c0a2b44052f2c4a
alexey.ivanes@gmail.com <alexey.ivanes@gmail.com@8ee3b2d0-dc07-11de-995a-ab19b33c1a40>
2012-09-27 22:01:38 +0000

    CMake: pkgconfig для сборки под linux

--- trunk/src/plugins/spellchecker/enchantchecker.cmake
+++ trunk/src/plugins/spellchecker/enchantchecker.cmake
@@ -1,12 +1,17 @@
 add_definitions(-DHAVE_ENCHANT) 
 
-if(SYSTEM_ENCHANT_FOUND)
-	set(ADD_LIBS enchant)
-else(SYSTEM_ENCHANT_FOUND)
-	message(FATAL_ERROR "Enchant not found, check enchant installation or use -DSPELLCHECKER_BACKEND=HUNSPELL instead")
-endif(SYSTEM_ENCHANT_FOUND)
+if (UNIX)
+	FIND_PACKAGE(PkgConfig)
+	pkg_check_modules(SYSTEM_ENCHANT enchant>=1.6.0)
 
-message(STATUS "Spellchecker backend: enchant")
+	if (SYSTEM_ENCHANT_FOUND)
+		set(ADD_LIBS ${SYSTEM_ENCHANT_LIBRARIES})
+		message(STATUS "Spellchecker backend: system enchant")
+	else (SYSTEM_ENCHANT_FOUND)
+		message(FATAL_ERROR "Enchant not found, check enchant installation or use -DSPELLCHECKER_BACKEND=HUNSPELL instead")
+		return()
+	endif (SYSTEM_ENCHANT_FOUND)
+endif (UNIX)
 
 set(SOURCES ${SOURCES} "enchantchecker.cpp")
 set(HEADERS ${HEADERS} "enchantchecker.h")
