c00c4b2767aed8494c95dde86f88a0387bde08cc
Alexey Ivanov <alexey.ivanes@gmail.com>
2012-09-28 14:53:13 +0000

    CMake: используем hunspell, если в системе не найден ecnhant, так как enchant не основная зависимость

--- src/plugins/spellchecker/enchantchecker.cmake
+++ src/plugins/spellchecker/enchantchecker.cmake
@@ -1,16 +1,8 @@
 add_definitions(-DHAVE_ENCHANT) 
 
 if (UNIX)
-	FIND_PACKAGE(PkgConfig)
-	pkg_check_modules(SYSTEM_ENCHANT enchant>=1.6.0)
-
-	if (SYSTEM_ENCHANT_FOUND)
-		set(ADD_LIBS ${SYSTEM_ENCHANT_LIBRARIES})
-		message(STATUS "Spellchecker backend: system enchant")
-	else (SYSTEM_ENCHANT_FOUND)
-		message(FATAL_ERROR "Enchant not found, check enchant installation or use -DSPELLCHECKER_BACKEND=HUNSPELL instead")
-		return()
-	endif (SYSTEM_ENCHANT_FOUND)
+	set(ADD_LIBS ${SYSTEM_ENCHANT_LIBRARIES})
+	message(STATUS "Spellchecker backend: system enchant")
 endif (UNIX)
 
 set(SOURCES ${SOURCES} "enchantchecker.cpp")
