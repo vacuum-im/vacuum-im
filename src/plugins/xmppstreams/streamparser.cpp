3f08ef7e381e22e31a066bad8d616be6d7395742
Sergey Potapov <potapov.s.a@gmail.com>
2013-01-15 09:28:37 +0000

    Убраны лишние определения namespaceURI в парсере XML потока.

--- src/plugins/xmppstreams/streamparser.cpp
+++ src/plugins/xmppstreams/streamparser.cpp
@@ -23,17 +23,36 @@ void StreamParser::parseData(const QByteArray &AData)
 		if (FReader.isStartDocument())
 		{
 			FLevel = 0;
+			FCurrentElem = QDomElement();
 		}
 		else if (FReader.isStartElement())
 		{
-			QDomElement newElement = doc.createElementNS(FReader.namespaceUri().toString(),FReader.qualifiedName().toString());
-			foreach(QXmlStreamAttribute attribute, FReader.attributes())
+			QMap<QStringRef, QStringRef> nsDeclarations;
+			foreach(const QXmlStreamNamespaceDeclaration &nsDecl, FReader.namespaceDeclarations())
+				nsDeclarations.insert(nsDecl.prefix(),nsDecl.namespaceUri());
+
+			QDomElement newElement;
+			if (nsDeclarations.contains(FReader.prefix()))
+				newElement = doc.createElementNS(FReader.namespaceUri().toString(),FReader.qualifiedName().toString());
+			else
+				newElement = doc.createElement(FReader.qualifiedName().toString());
+
+			foreach(const QXmlStreamAttribute &attribute, FReader.attributes())
 			{
 				QString attrNs = attribute.namespaceUri().toString();
 				if (!attrNs.isEmpty())
 					newElement.setAttributeNS(attrNs,attribute.qualifiedName().toString(),attribute.value().toString());
 				else
 					newElement.setAttribute(attribute.qualifiedName().toString(),attribute.value().toString());
+			}
+
+			for(QMap<QStringRef, QStringRef>::const_iterator it=nsDeclarations.constBegin(); it!=nsDeclarations.constEnd(); ++it)
+			{
+				if (it.key() != FReader.prefix())
+				{
+					QString prefix = it.key().toString();
+					newElement.setAttribute(!prefix.isEmpty() ? prefix+QString(":xmlns") : QString("xmlns"), it->toString());
+				}
 			}
 
 			FLevel++;
@@ -60,12 +79,12 @@ void StreamParser::parseData(const QByteArray &AData)
 		else if (FReader.isEndElement())
 		{
 			FLevel--;
-			if (FLevel == 0)
-				emit closed();
+			if (FLevel > 1)
+				FCurrentElem = FCurrentElem.parentNode().toElement();
 			else if (FLevel == 1)
 				emit element(FRootElem);
-			else if (FLevel > 1)
-				FCurrentElem = FCurrentElem.parentNode().toElement();
+			else if (FLevel == 0)
+				emit closed();
 		}
 	}
 
