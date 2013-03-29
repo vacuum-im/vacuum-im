09001660de9f4d54c8663b1dafbd3d3bf0558957
Sergey Potapov <potapov.s.a@gmail.com>
2013-03-29 10:38:23 +0000

    Добавлены файлы локализаций для плагина статистики.

---
+++ src/translations/en/statistics.ts
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en" sourcelanguage="en">
+<context>
+    <name>Statistics</name>
+    <message>
+        <source>Statistics</source>
+        <translation>Statistics</translation>
+    </message>
+    <message>
+        <source>Allows to collect application statistics</source>
+        <translation>Allows to collect application statistics</translation>
+    </message>
+</context>
+</TS>
---
+++ src/translations/pl/statistics.ts
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pl" sourcelanguage="en">
+<context>
+    <name>Statistics</name>
+    <message>
+        <source>Statistics</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Allows to collect application statistics</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
