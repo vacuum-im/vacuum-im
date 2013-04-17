80639fed9085bb6b776c79701b1b5a2af993eb9e
Sergey Potapov <potapov.s.a@gmail.com>
2013-04-17 19:45:21 +0000

    Приложение завершалось с ошибкой при удалении из истории не завершенного разговора.

--- src/plugins/filemessagearchive/collectionwriter.cpp
+++ src/plugins/filemessagearchive/collectionwriter.cpp
@@ -76,8 +76,6 @@ bool CollectionWriter::writeMessage(const Message &AMessage, const QString &ASav
 		if (!FGroupchat || !contactJid.resource().isEmpty())
 		{
 			FMessagesCount++;
-			FCloseTimer.stop();
-
 			FXmlWriter->writeStartElement(ADirectionIn ? "from" : "to");
 
 			int secs = FHeader.start.secsTo(AMessage.dateTime());
@@ -114,7 +112,6 @@ bool CollectionWriter::writeNote(const QString &ANote)
 	if (isOpened() && !ANote.isEmpty())
 	{
 		FNotesCount++;
-		FCloseTimer.stop();
 		FXmlWriter->writeStartElement("note");
 		FXmlWriter->writeAttribute("utc",DateTime(QDateTime::currentDateTime()).toX85UTC());
 		FXmlWriter->writeCharacters(ANote);
@@ -126,9 +123,14 @@ bool CollectionWriter::writeNote(const QString &ANote)
 	return false;
 }
 
+void CollectionWriter::closeAndDeleteLater()
+{
+	stopCollection();
+	deleteLater();
+}
+
 void CollectionWriter::startCollection()
 {
-	FCloseTimer.stop();
 	FXmlWriter->setAutoFormatting(true);
 	FXmlWriter->writeStartElement("chat");
 	FXmlWriter->writeAttribute("with",FHeader.with.full());
@@ -143,7 +145,6 @@ void CollectionWriter::startCollection()
 
 void CollectionWriter::stopCollection()
 {
-	FCloseTimer.stop();
 	if (FXmlWriter)
 	{
 		FXmlWriter->writeEndElement();
@@ -154,12 +155,10 @@ void CollectionWriter::stopCollection()
 	if (FXmlFile)
 	{
 		FXmlFile->close();
-		delete FXmlFile;
+		if (FMessagesCount == 0)
+			QFile::remove(FFileName);
+		FXmlFile->deleteLater();
 		FXmlFile = NULL;
-	}
-	if (FMessagesCount == 0)
-	{
-		QFile::remove(FFileName);
 	}
 }
 
@@ -201,7 +200,7 @@ void CollectionWriter::checkLimits()
 	else if (FXmlFile->size() > Options::node(OPV_FILEARCHIVE_COLLECTION_MAXSIZE).value().toInt())
 		FCloseTimer.start(0);
 	else if (FMessagesCount > Options::node(OPV_FILEARCHIVE_COLLECTION_MINMESSAGES).value().toInt())
-	   FCloseTimer.start(Options::node(OPV_FILEARCHIVE_COLLECTION_TIMEOUT).value().toInt());
+		FCloseTimer.start(Options::node(OPV_FILEARCHIVE_COLLECTION_TIMEOUT).value().toInt());
 	else
 		FCloseTimer.start(Options::node(OPV_FILEARCHIVE_COLLECTION_MAXTIMEOUT).value().toInt());
 }
