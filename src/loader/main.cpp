c586f9a27f4d94e4e3bd45609b7c3af3bcd881bf
Alexey Ivanov <alexey.ivanes@gmail.com>
2015-06-02 03:54:18 +0300

    Поддержка OSX 10.10, хак для системного шрифта интерфейса в OSX 10.10

--- src/loader/main.cpp
+++ src/loader/main.cpp
@@ -4,6 +4,13 @@
 
 int main(int argc, char *argv[])
 {
+#ifdef Q_OS_MACX
+	if (QSysInfo::MacintoshVersion == QSysInfo::MV_YOSEMITE )
+	{
+		// https://bugreports.qt-project.org/browse/QTBUG-40833
+		QFont::insertSubstitution(".Helvetica Neue DeskInterface", "Helvetica Neue");
+	}
+#endif
 	QApplication app(argc, argv);
 	app.setQuitOnLastWindowClosed(false);
 	app.addLibraryPath(app.applicationDirPath());
