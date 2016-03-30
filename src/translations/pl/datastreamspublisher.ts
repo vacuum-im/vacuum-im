0ff63291b770dd02ce7d0d525a265cb818c23830
Sergey Potapov <potapov.s.a@gmail.com>
2016-03-30 21:19:19 +0300

    Добавлены забытые локализации плагина datastreamspublisher.

---
+++ src/translations/pl/datastreamspublisher.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="pl" sourcelanguage="en" version="2.0">
+<context>
+    <name>DataStreamsPublisher</name>
+    <message>
+        <source>Data Streams Publisher</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Allows to publish available data streams</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Data Streams Publication</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Supports the publication of the data streams</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+</TS>
