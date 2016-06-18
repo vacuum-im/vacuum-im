ba1a0da7a1bf6d005b3b522390be408dfbf8600c
Sergey Potapov <potapov.s.a@gmail.com>
2016-06-18 16:08:21 +0300

    Исправлена критическая ошибка при удалении более одного уведомления в спике участников конференции.

--- src/plugins/multiuserchat/multiuserview.cpp
+++ src/plugins/multiuserchat/multiuserview.cpp
@@ -343,9 +343,10 @@ void MultiUserView::removeItemNotify(int ANotifyId)
 	{
 		LOG_STRM_DEBUG(FMultiChat->streamJid(),QString("Removing item notify, notify=%1, room=%2").arg(ANotifyId).arg(FMultiChat->roomJid().bare()));
 
-		QStandardItem *item = FItemNotifies.key(ANotifyId);
 		FNotifies.remove(ANotifyId);
-		FItemNotifies.remove(item);
+
+		QStandardItem *item = FItemNotifies.key(ANotifyId);
+		FItemNotifies.remove(item,ANotifyId);
 		updateItemNotify(item);
 
 		emit itemNotifyRemoved(ANotifyId);
