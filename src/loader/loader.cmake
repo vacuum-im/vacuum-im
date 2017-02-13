37f19db638c73a09d54ba16e7667e61d483af72b
Alexey Ivanov <alexey.ivanes@gmail.com>
2017-02-13 14:16:15 +0300

    Branch for cmake changes/improvements:  - qt_use_modules macro deprecated, replaced with target_link_libraries  - CMAKE_AUTOMOC and CMAKE_AUTOUIC used instead of manual qt5_warp_moc/uic  - removed old .svn exclude pattern

--- src/loader/loader.cmake
+++ src/loader/loader.cmake
@@ -4,10 +4,3 @@ file(GLOB UIS "*.ui")
 set(HEADERS "aboutbox.h"
             "pluginmanager.h"
             "setuppluginsdialog.h")
-
-qt5_wrap_cpp(MOC_SOURCES ${HEADERS})
-qt5_wrap_ui(UI_HEADERS ${UIS})
-
-if (NOT MSVC)
-	qt5_add_resources(RC_SOURCES "loader.rc")
-endif (NOT MSVC)
