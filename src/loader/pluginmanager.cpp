8fb975be706c272e623fca7d9d3cb2785b88d83f
Alexey Ivanov <alexey.ivanes@gmail.com>
2017-02-17 17:08:32 +0300

    pluginmanager #define fix

--- src/loader/pluginmanager.cpp
+++ src/loader/pluginmanager.cpp
@@ -44,7 +44,7 @@
 #if defined(Q_OS_WIN)
 #  define ENV_APP_DATA              "APPDATA"
 #  define DIR_APP_DATA              APPLICATION_NAME
-#  define PATH_APP_DATA             ORGANIZATION_NAME"/"DIR_APP_DATA
+#  define PATH_APP_DATA             ORGANIZATION_NAME "/" DIR_APP_DATA
 #elif defined(Q_OS_LINUX)
 #  define ENV_APP_DATA              "HOME"
 #  define DIR_APP_DATA              ".vacuum"
@@ -52,11 +52,11 @@
 #elif defined(Q_OS_MAC)
 #  define ENV_APP_DATA              "HOME"
 #  define DIR_APP_DATA              APPLICATION_NAME
-#  define PATH_APP_DATA             "Library/Application Support/"DIR_APP_DATA
+#  define PATH_APP_DATA             "Library/Application Support/" DIR_APP_DATA
 #elif defined(Q_WS_HAIKU)
 #  define ENV_APP_DATA              "APPDATA"
 #  define DIR_APP_DATA              APPLICATION_NAME
-#  define PATH_APP_DATA             ORGANIZATION_NAME"/"DIR_APP_DATA
+#  define PATH_APP_DATA             ORGANIZATION_NAME "/" DIR_APP_DATA
 #endif
 
 #if defined(Q_OS_WIN)
