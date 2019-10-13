24448eddab9251964425a619ac1077d2025c0773
Alexey Ivanov <alexey.ivanes@gmail.com>
2019-10-13 22:06:58 +0300

    Initial support for SCRAM-SHA1 https://tools.ietf.org/html/rfc5802 (#962)
    * Initial support for SCRAM-SHA1 https://tools.ietf.org/html/rfc5802

--- src/plugins/saslauth/saslauthfeature.h
+++ src/plugins/saslauth/saslauthfeature.h
@@ -33,6 +33,13 @@ protected slots:
 private:
 	IXmppStream *FXmppStream;
 	QStringList FMechanisms;
+	QString FSelectedMechanism;
+	QByteArray SCRAMSHA1_clientNonce;
+	QByteArray SCRAMSHA1_initialMessage;
+	QByteArray SCRAMSHA1_GS2Header;
+	QByteArray SCRAMSHA1_ServerSignature;
+
+	bool challengeResponseError(const Jid &streamJid, const Stanza &AStanza);
 };
 
 #endif // SASLAUTHFEATURE_H
