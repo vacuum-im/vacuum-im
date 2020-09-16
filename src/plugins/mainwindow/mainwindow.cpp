cdd4ed89f8d18bf1daf139f11cf9db5ac094414e
Alexey Ivanov <alexey.ivanes@gmail.com>
2020-09-16 22:00:37 +0300

    fix for buggy "one-window-mode" geometry restore

--- src/plugins/mainwindow/mainwindow.cpp
+++ src/plugins/mainwindow/mainwindow.cpp
@@ -396,7 +396,7 @@ void MainWindow::onCurrentCentralPageChanged()
 void MainWindow::onCentralPageAddedOrRemoved(IMainCentralPage *APage)
 {
 	Q_UNUSED(APage);
-	QTimer::singleShot(0,this,SLOT(onUpdateCentralWidgetVisible()));
+	onUpdateCentralWidgetVisible();
 }
 
 void MainWindow::onSplitterMoved(int APos, int AIndex)
